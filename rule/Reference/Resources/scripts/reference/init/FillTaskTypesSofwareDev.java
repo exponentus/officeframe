17190a90de242acbd2879ecd2ebdad4480297d84
k-zon_000 <k-zon_000@KayraNote>
2016-04-21 13:18:22 +0300

    1

---
+++ rule/Reference/Resources/scripts/reference/init/FillTaskTypesSofwareDev.java
@@ -0,0 +1,46 @@
+package reference.init;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.exponentus.dataengine.jpa.deploying.InitialDataAdapter;
+import com.exponentus.localization.LanguageCode;
+import com.exponentus.scripting._Session;
+
+import kz.flabs.localization.Vocabulary;
+import reference.dao.TaskTypeDAO;
+import reference.model.TaskType;
+
+/**
+ * @author Kayra on 21/04/16.
+ */
+
+public class FillTaskTypesSofwareDev extends InitialDataAdapter<TaskType, TaskTypeDAO> {
+
+	@Override
+	public List<TaskType> getData(_Session ses, LanguageCode lang, Vocabulary vocabulary) {
+		List<TaskType> entities = new ArrayList<TaskType>();
+		String[] data = { "Документирование", "Техническая поддержка", "Тестирование", "Программированние" };
+		String[] dataEng = { "Documentation", "Technical support", "Testing", "Programming" };
+
+		for (int i = 0; i < data.length; i++) {
+			TaskType entity = new TaskType();
+			entity.setName(data[i]);
+			Map<LanguageCode, String> name = new HashMap<LanguageCode, String>();
+			name.put(LanguageCode.ENG, dataEng[i]);
+			name.put(LanguageCode.RUS, data[i]);
+			entity.setLocalizedName(name);
+			entities.add(entity);
+		}
+
+		return entities;
+	}
+
+	@Override
+	public Class<TaskTypeDAO> getDAO() {
+		return TaskTypeDAO.class;
+	}
+
+}
