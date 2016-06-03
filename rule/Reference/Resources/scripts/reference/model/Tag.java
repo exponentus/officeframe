259b0481f0540c7970e67bfd93466fdec2f50860
k-zon_000 <k-zon_000@KayraNote>
2016-06-03 11:12:59 +0300

    #

--- rule/Reference/Resources/scripts/reference/model/Tag.java
+++ rule/Reference/Resources/scripts/reference/model/Tag.java
@@ -1,93 +1,104 @@
 package reference.model;
 
-import administrator.dao.LanguageDAO;
-import administrator.model.Language;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import com.exponentus.common.model.SimpleReferenceEntity;
 import com.exponentus.scripting._Session;
 import com.exponentus.util.Util;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.*;
-import java.util.List;
-import java.util.UUID;
+import administrator.dao.LanguageDAO;
+import administrator.model.Language;
 
 @Entity
-@Table(name = "tags", uniqueConstraints = @UniqueConstraint(columnNames = {"parent", "name"}))
+@Table(name = "tags", uniqueConstraints = @UniqueConstraint(columnNames = { "parent", "name" }) )
 @NamedQuery(name = "Tag.findAll", query = "SELECT m FROM Tag AS m WHERE m.parent IS NULL ORDER BY m.name")
 public class Tag extends SimpleReferenceEntity {
 
-    @Column(length = 7)
-    private String color;
+	@Column(length = 7)
+	private String color;
 
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent")
-    private Tag parent;
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent")
+	private Tag parent;
 
-    @OneToMany(mappedBy = "parent", cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
-    private List<Tag> children;
+	@OneToMany(mappedBy = "parent", cascade = { CascadeType.ALL }, fetch = FetchType.LAZY)
+	private List<Tag> children;
 
-    public Tag getParent() {
-        return parent;
-    }
+	public Tag getParent() {
+		return parent;
+	}
 
-    public void setParent(Tag parent) {
-        this.parent = parent;
-    }
+	public void setParent(Tag parent) {
+		this.parent = parent;
+	}
 
-    public List<Tag> getChildren() {
-        return children;
-    }
+	public List<Tag> getChildren() {
+		return children;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public UUID getParentId() {
-        if (parent == null) {
-            return null;
-        }
-        return parent.id;
-    }
+	public UUID getParentId() {
+		if (parent == null) {
+			return null;
+		}
+		return parent.id;
+	}
 
-    public void setParentId(UUID id) {
-        if (id == null) {
-            setParent(null);
-            return;
-        }
+	public void setParentId(UUID id) {
+		if (id == null) {
+			setParent(null);
+			return;
+		}
 
-        Tag parent = new Tag();
-        parent.setId(id);
-        setParent(parent);
-    }
+		Tag parent = new Tag();
+		parent.setId(id);
+		setParent(parent);
+	}
 
-    @Override
-    public String getFullXMLChunk(_Session ses) {
-        StringBuilder chunk = new StringBuilder(1000);
-        chunk.append("<regdate>" + Util.simpleDateFormat.format(regDate) + "</regdate>");
-        chunk.append("<name>" + getName() + "</name>");
-        chunk.append("<color>" + color + "</color>");
-        chunk.append("<localizednames>");
-        LanguageDAO lDao = new LanguageDAO(ses);
-        List<Language> list = lDao.findAll();
-        for (Language l : list) {
-            chunk.append("<entry id=\"" + l.getCode() + "\">" + getLocalizedName(l.getCode()) + "</entry>");
-        }
-        chunk.append("</localizednames>");
-        return chunk.toString();
-    }
+	@Override
+	public String getFullXMLChunk(_Session ses) {
+		StringBuilder chunk = new StringBuilder(1000);
+		chunk.append("<regdate>" + Util.dateFormat.format(regDate) + "</regdate>");
+		chunk.append("<name>" + getName() + "</name>");
+		chunk.append("<color>" + color + "</color>");
+		chunk.append("<localizednames>");
+		LanguageDAO lDao = new LanguageDAO(ses);
+		List<Language> list = lDao.findAll();
+		for (Language l : list) {
+			chunk.append("<entry id=\"" + l.getCode() + "\">" + getLocalizedName(l.getCode()) + "</entry>");
+		}
+		chunk.append("</localizednames>");
+		return chunk.toString();
+	}
 
-    @Override
-    public String getShortXMLChunk(_Session ses) {
-        return "<name>" + getLocalizedName(ses.getLang()) + "</name><color>" + color + "</color>";
-    }
+	@Override
+	public String getShortXMLChunk(_Session ses) {
+		return "<name>" + getLocalizedName(ses.getLang()) + "</name><color>" + color + "</color>";
+	}
 
-    @Override
-    public String toString() {
-        return "Tag[" + id + ", " + getLocalizedName() + ", " + color + ", " + parent + ", " + getAuthor() + ", " + getRegDate() + "]";
-    }
+	@Override
+	public String toString() {
+		return "Tag[" + id + ", " + getLocalizedName() + ", " + color + ", " + parent + ", " + getAuthor() + ", " + getRegDate() + "]";
+	}
 }
--- rule/Staff/Resources/scripts/staff/model/Department.java
+++ rule/Staff/Resources/scripts/staff/model/Department.java
@@ -1,17 +1,24 @@
 package staff.model;
 
-import administrator.dao.LanguageDAO;
-import administrator.model.Language;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import com.exponentus.common.model.SimpleReferenceEntity;
 import com.exponentus.scripting._Session;
 import com.exponentus.util.Util;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import reference.model.DepartmentType;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.List;
+import administrator.dao.LanguageDAO;
+import administrator.model.Language;
+import reference.model.DepartmentType;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
@@ -19,58 +26,58 @@ import java.util.List;
 @NamedQuery(name = "Department.findAll", query = "SELECT m FROM Department AS m ORDER BY m.regDate")
 public class Department extends SimpleReferenceEntity {
 
-    @ManyToOne(optional = false)
-    @JoinColumn(nullable = false)
-    private DepartmentType type;
+	@ManyToOne(optional = false)
+	@JoinColumn(nullable = false)
+	private DepartmentType type;
 
-    @NotNull
-    @ManyToOne(optional = false)
-    @JoinColumn(nullable = false)
-    private Organization organization;
+	@NotNull
+	@ManyToOne(optional = false)
+	@JoinColumn(nullable = false)
+	private Organization organization;
 
-    @OneToMany(mappedBy = "department")
-    private List<Employee> employees;
+	@OneToMany(mappedBy = "department")
+	private List<Employee> employees;
 
-    @JsonIgnore
-    public Organization getOrganization() {
-        return organization;
-    }
+	@JsonIgnore
+	public Organization getOrganization() {
+		return organization;
+	}
 
-    public void setOrganization(Organization organization) {
-        this.organization = organization;
-    }
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
+	}
 
-    @JsonIgnore
-    public List<Employee> getEmployees() {
-        return employees;
-    }
+	@JsonIgnore
+	public List<Employee> getEmployees() {
+		return employees;
+	}
 
-    public DepartmentType getType() {
-        return type;
-    }
+	public DepartmentType getType() {
+		return type;
+	}
 
-    public void setType(DepartmentType type) {
-        this.type = type;
-    }
+	public void setType(DepartmentType type) {
+		this.type = type;
+	}
 
-    String getOrganizationId() {
-        return organization.getIdentifier();
-    }
+	String getOrganizationId() {
+		return organization.getIdentifier();
+	}
 
-    @Override
-    public String getFullXMLChunk(_Session ses) {
-        StringBuilder chunk = new StringBuilder(1000);
-        chunk.append("<regdate>" + Util.simpleDateFormat.format(regDate) + "</regdate>");
-        chunk.append("<name>" + getName() + "</name>");
-        chunk.append("<type>" + getType() + "</type>");
-        chunk.append("<organization id=\"" + organization.getId() + "\">" + organization.getLocalizedName(ses.getLang()) + "</organization>");
-        chunk.append("<localizednames>");
-        LanguageDAO lDao = new LanguageDAO(ses);
-        List<Language> list = lDao.findAll();
-        for (Language l : list) {
-            chunk.append("<entry id=\"" + l.getCode() + "\">" + getLocalizedName(l.getCode()) + "</entry>");
-        }
-        chunk.append("</localizednames>");
-        return chunk.toString();
-    }
+	@Override
+	public String getFullXMLChunk(_Session ses) {
+		StringBuilder chunk = new StringBuilder(1000);
+		chunk.append("<regdate>" + Util.dateFormat.format(regDate) + "</regdate>");
+		chunk.append("<name>" + getName() + "</name>");
+		chunk.append("<type>" + getType() + "</type>");
+		chunk.append("<organization id=\"" + organization.getId() + "\">" + organization.getLocalizedName(ses.getLang()) + "</organization>");
+		chunk.append("<localizednames>");
+		LanguageDAO lDao = new LanguageDAO(ses);
+		List<Language> list = lDao.findAll();
+		for (Language l : list) {
+			chunk.append("<entry id=\"" + l.getCode() + "\">" + getLocalizedName(l.getCode()) + "</entry>");
+		}
+		chunk.append("</localizednames>");
+		return chunk.toString();
+	}
 }
