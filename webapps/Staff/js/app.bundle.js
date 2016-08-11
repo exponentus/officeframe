190a985f4be810776c3bf14ffbbbee5306abcad1
John <john_198708@mail.ru>
2016-08-11 22:35:11 +0600

    commit

--- webapps/Staff/js/app.bundle.js
+++ webapps/Staff/js/app.bundle.js
@@ -3159,14 +3159,28 @@ $(function() {
 
 nbApp.selectOptions = {
     organization: {
-        url: 'p?id=get-organizations'
+        url: 'p?id=get-organizations',
+        onSelect: function(e) {
+            if(e.params.data != undefined){
+                $("select[name='department'], select[name='organization']").prop('required', false);
+                $("select[name='department'], select[name='organization']").removeClass("required");
+                $("select[name='department'], select[name='organization']").closest(".controls").removeClass("has-error")
+            }
+        }
     },
     orgcategory: {
         url: 'p?id=get-org-categories'
     },
     department: {
         url: 'p?id=get-departments',
-        data: ['organization']
+        data: ['organization'],
+    onSelect: function(e) {
+        if(e.params.data != undefined) {
+            $("select[name='department'], select[name='organization']").prop('required', false);
+            $("select[name='department'], select[name='organization']").removeClass("required");
+            $("select[name='department'], select[name='organization']").closest(".controls").removeClass("has-error")
+        }
+    }
     },
     position: {
         url: 'p?id=get-positions'
