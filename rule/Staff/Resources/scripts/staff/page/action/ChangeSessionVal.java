2f3e0ebe428db75d75feb40d269947137449291b
medin84 <kalihanovm@gmail.com>
2016-09-29 13:54:37 +0600

    commit

--- rule/Staff/Resources/scripts/staff/page/action/ChangeSessionVal.java
+++ rule/Staff/Resources/scripts/staff/page/action/ChangeSessionVal.java
@@ -4,19 +4,18 @@ import com.exponentus.localization.LanguageCode;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.event._DoPage;
-import com.exponentus.util.Util;
 
 public class ChangeSessionVal extends _DoPage {
-	@Override
-	public void doPOST(_Session session, _WebFormData formData) {
-		String pageSize = formData.getValueSilently("pagesize");
-		String lang = formData.getValueSilently("lang");
+    @Override
+    public void doPOST(_Session session, _WebFormData formData) {
+        int pageSize = formData.getNumberValueSilently("pagesize", 0);
+        String lang = formData.getValueSilently("lang");
 
-		if (!pageSize.isEmpty()) {
-			session.pageSize = Util.convertStringToInt(pageSize);
-		}
-		if (!lang.isEmpty()) {
-			session.setLang(LanguageCode.valueOf(lang));
-		}
-	}
+        if (pageSize > 9 && pageSize < 1001) {
+            session.pageSize = pageSize;
+        }
+        if (!lang.isEmpty()) {
+            session.setLang(LanguageCode.valueOf(lang));
+        }
+    }
 }
