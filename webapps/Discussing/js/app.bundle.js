b69feac71b87ab0c41dbe99390e276a2bcb2f344
John <john_198708@mail.ru>
2016-09-30 18:38:46 +0600

    commit

--- webapps/Discussing/js/app.bundle.js
+++ webapps/Discussing/js/app.bundle.js
@@ -5440,6 +5440,25 @@ $(function() {
         event.preventDefault();
         nb.submitForm(nb.getForm(this));
     });
+
+    $('[data-action=delete_document]').click(function(event) {
+        event.preventDefault();
+
+        var docids = nb.getSelectedEntityIDs('docid');
+        if (!docids.length) {
+            return;
+        }
+
+        nb.xhrDelete(location.href + '&docid=' + docids.join('&docid=')).then(function() {
+            location.reload();
+        });
+    });
+
+    $('[data-action=delete_document]').attr('disabled', true);
+    $(':checkbox').bind('change', function() {
+        var countChecked = $('[name=docid]:checked').length;
+        $('[data-action=delete_document]').attr('disabled', countChecked === 0);
+    });
 });
 
 $(document).ready(function() {
