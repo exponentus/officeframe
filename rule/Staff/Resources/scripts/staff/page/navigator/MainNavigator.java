07be9e0ff5bcb643db169ef96d231dc70e1d4024
justkayra <biosubj@gmail.com>
2016-11-05 18:33:30 +0200

    #

--- rule/Staff/Resources/scripts/staff/page/navigator/MainNavigator.java
+++ rule/Staff/Resources/scripts/staff/page/navigator/MainNavigator.java
@@ -22,44 +22,53 @@ public class MainNavigator extends _DoPage {
 
 	@Override
 	public void doGET(_Session session, _WebFormData formData) {
-		LanguageCode lang = session.getLang();
-		LinkedList<IOutcomeObject> list = new LinkedList<>();
+		final LanguageCode lang = session.getLang();
+		final LinkedList<IOutcomeObject> list = new LinkedList<>();
 
-		_Outline common_outline = new _Outline(getLocalizedWord("common_staff_data", lang), "common");
-		_OutlineEntry primaryOrgEntry = new _OutlineEntry(getLocalizedWord("primary_organization", lang), "primaryorg-view");
-		OrganizationDAO oDao = new OrganizationDAO(session);
-		Organization primaryOrg = oDao.findPrimaryOrg();
+		final _Outline common_outline = new _Outline(getLocalizedWord("common_staff_data", lang), "common");
+
+		final OrganizationDAO oDao = new OrganizationDAO(session);
+		final Organization primaryOrg = oDao.findPrimaryOrg();
+		_OutlineEntry primaryOrgEntry = null;
 		if (primaryOrg != null) {
+			primaryOrgEntry = new _OutlineEntry(getLocalizedWord("primary_organization", lang),
+					"primaryorg-view");
 			primaryOrgEntry.addEntry(new _OutlineEntry(primaryOrg.getLocalizedName(lang),
-			        getLocalizedWord("primary_organization", lang) + " : " + primaryOrg.getLocalizedName(lang), "structure-view",
-			        "p?id=structure-view"));
+					getLocalizedWord("primary_organization", lang) + " : " + primaryOrg.getLocalizedName(lang),
+					"structure-view", "p?id=structure-view"));
 		}
-		_OutlineEntry orgEntry = new _OutlineEntry(getLocalizedWord("organizations", lang), "organization-view");
-		for (OrganizationLabel label : new OrganizationLabelDAO(session).findAll()) {
-			orgEntry.addEntry(
-			        new _OutlineEntry(label.getLocalizedName(lang), getLocalizedWord("labeled", lang) + " : " + label.getLocalizedName(lang),
-			                "organization-label-view" + label.getId(), "p?id=organization-label-view&categoryid=" + label.getId()));
+		final _OutlineEntry orgEntry = new _OutlineEntry(getLocalizedWord("organizations", lang), "organization-view");
+		for (final OrganizationLabel label : new OrganizationLabelDAO(session).findAll()) {
+			orgEntry.addEntry(new _OutlineEntry(label.getLocalizedName(lang),
+					getLocalizedWord("labeled", lang) + " : " + label.getLocalizedName(lang),
+					"organization-label-view" + label.getId(),
+					"p?id=organization-label-view&categoryid=" + label.getId()));
 		}
-		_OutlineEntry departmentEntry = new _OutlineEntry(getLocalizedWord("departments", lang), "department-view");
-		_OutlineEntry employeeEntry = new _OutlineEntry(getLocalizedWord("employees", lang), "employee-view");
-		for (Role role : new RoleDAO(session).findAll()) {
-			employeeEntry
-			        .addEntry(new _OutlineEntry(role.getLocalizedName(lang), getLocalizedWord("assigned", lang) + " : " + role.getLocalizedName(lang),
-			                "role-view" + role.getId(), "p?id=role-view&categoryid=" + role.getId()));
+		final _OutlineEntry departmentEntry = new _OutlineEntry(getLocalizedWord("departments", lang),
+				"department-view");
+		final _OutlineEntry employeeEntry = new _OutlineEntry(getLocalizedWord("employees", lang), "employee-view");
+		for (final Role role : new RoleDAO(session).findAll()) {
+			employeeEntry.addEntry(new _OutlineEntry(role.getLocalizedName(lang),
+					getLocalizedWord("assigned", lang) + " : " + role.getLocalizedName(lang),
+					"role-view" + role.getId(), "p?id=role-view&categoryid=" + role.getId()));
 		}
 
-		common_outline.addEntry(primaryOrgEntry);
+		if (primaryOrgEntry != null) {
+			common_outline.addEntry(primaryOrgEntry);
+		}
 		common_outline.addEntry(orgEntry);
 		common_outline.addEntry(departmentEntry);
 		common_outline.addEntry(employeeEntry);
 
 		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("roles", lang), "role-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("organization_labels", lang), "organization-label-view"));
+		common_outline
+		.addEntry(new _OutlineEntry(getLocalizedWord("organization_labels", lang), "organization-label-view"));
 
 		list.add(common_outline);
 
 		addValue("workspaceUrl", Environment.getWorkspaceURL());
-		addValue("outline_current", formData.getValueSilently("id").replace("-form", "-view") + formData.getValueSilently("categoryid"));
+		addValue("outline_current",
+				formData.getValueSilently("id").replace("-form", "-view") + formData.getValueSilently("categoryid"));
 		addContent(list);
 	}
 }
