39dde90eaa5a0d44aaef7d1c0ebec5ab81b768b7
kayra <justaidajam@gmail.com>
2016-11-11 00:42:49 +0200

    #

--- rule/Reference/Resources/scripts/reference/page/form/TextTemplateForm.java
+++ rule/Reference/Resources/scripts/reference/page/form/TextTemplateForm.java
@@ -1,6 +1,9 @@
 package reference.page.form;
 
-import administrator.dao.LanguageDAO;
+import java.util.UUID;
+
+import org.eclipse.persistence.exceptions.DatabaseException;
+
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.exception.SecureException;
 import com.exponentus.scripting._Exception;
@@ -8,14 +11,13 @@ import com.exponentus.scripting._Session;
 import com.exponentus.scripting._Validation;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.user.IUser;
-import org.eclipse.persistence.exceptions.DatabaseException;
+
+import administrator.dao.LanguageDAO;
 import reference.dao.TextTemplateDAO;
 import reference.model.TextTemplate;
 
-import java.util.UUID;
-
 public class TextTemplateForm extends ReferenceForm {
-
+	
 	@Override
 	public void doGET(_Session session, _WebFormData formData) {
 		String id = formData.getValueSilently("docid");
@@ -26,12 +28,13 @@ public class TextTemplateForm extends ReferenceForm {
 			entity = dao.findById(UUID.fromString(id));
 		} else {
 			entity = (TextTemplate) getDefaultEntity(user, new TextTemplate());
+			entity.setCategory("");
 		}
 		addContent(entity);
 		addContent(new LanguageDAO(session).findAll());
 		addContent(getSimpleActionBar(session));
 	}
-
+	
 	@Override
 	public void doPOST(_Session session, _WebFormData formData) {
 		try {
@@ -41,28 +44,28 @@ public class TextTemplateForm extends ReferenceForm {
 				setValidation(ve);
 				return;
 			}
-
+			
 			String id = formData.getValueSilently("docid");
 			TextTemplateDAO dao = new TextTemplateDAO(session);
 			TextTemplate entity;
 			boolean isNew = id.isEmpty();
-
+			
 			if (isNew) {
 				entity = new TextTemplate();
 			} else {
 				entity = dao.findById(UUID.fromString(id));
 			}
-
+			
 			entity.setName(formData.getValue("name"));
 			entity.setCategory(formData.getValue("category"));
 			entity.setLocalizedName(getLocalizedNames(session, formData));
-
+			
 			if (isNew) {
 				dao.add(entity);
 			} else {
 				dao.update(entity);
 			}
-
+			
 		} catch (_Exception | DatabaseException | SecureException | DAOException e) {
 			logError(e);
 		}
