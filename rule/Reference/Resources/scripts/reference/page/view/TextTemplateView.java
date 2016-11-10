ff8ee5db1c26f753cf6556c1456008b5c00d6a2a
John <john_198708@mail.ru>
2016-11-10 11:35:31 +0600

    commit

---
+++ rule/Reference/Resources/scripts/reference/page/view/TextTemplateView.java
@@ -0,0 +1,48 @@
+package reference.page.view;
+
+import com.exponentus.exception.SecureException;
+import com.exponentus.scripting._Session;
+import com.exponentus.scripting._WebFormData;
+import com.exponentus.scripting.actions._Action;
+import com.exponentus.scripting.actions._ActionBar;
+import com.exponentus.scripting.actions._ActionType;
+import com.exponentus.scripting.event._DoPage;
+import com.exponentus.user.IUser;
+import com.exponentus.user.SuperUser;
+import reference.dao.TextTemplateDAO;
+import reference.model.TextTemplate;
+
+import java.util.UUID;
+
+public class TextTemplateView extends _DoPage {
+
+	@Override
+	public void doGET(_Session session, _WebFormData formData) {
+		IUser<Long> user = session.getUser();
+		if (user.getId() == SuperUser.ID || user.getRoles().contains("reference_admin")) {
+			_ActionBar actionBar = new _ActionBar(session);
+			_Action newDocAction = new _Action(getLocalizedWord("new_", session.getLang()), "", "new_texttemplate");
+			newDocAction.setURL("Provider?id=texttemplate-form");
+			actionBar.addAction(newDocAction);
+			actionBar.addAction(new _Action(getLocalizedWord("del_document", session.getLang()), "", _ActionType.DELETE_DOCUMENT));
+
+			addContent(actionBar);
+		}
+		addContent(getViewPage(new TextTemplateDAO(session), formData));
+	}
+
+	@Override
+	public void doDELETE(_Session session, _WebFormData formData) {
+		println(formData);
+
+		TextTemplateDAO dao = new TextTemplateDAO(session);
+		for (String id : formData.getListOfValuesSilently("docid")) {
+			TextTemplate m = dao.findById(UUID.fromString(id));
+			try {
+				dao.delete(m);
+			} catch (SecureException e) {
+				setError(e);
+			}
+		}
+	}
+}
