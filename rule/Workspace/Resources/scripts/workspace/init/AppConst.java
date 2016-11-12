03b84b94dbfa7dac18237b191ac52157c5f8b50b
kayra <justaidajam@gmail.com>
2016-11-12 13:25:26 +0200

    #

--- rule/Workspace/Resources/scripts/workspace/init/AppConst.java
+++ rule/Workspace/Resources/scripts/workspace/init/AppConst.java
@@ -1,9 +1,11 @@
 package workspace.init;
 
+import com.exponentus.common.init.DefaultAppConst;
 import com.exponentus.dataengine.jpa.constants.AppCode;
 
-public class AppConst {
+public class AppConst extends DefaultAppConst {
 	public static String MODULE_VERSION = "1.0";
+	public static int VISIBILITY = 0;
 	public static String NAME = "Workspace";
 	public static String NAME_ENG = "Workspace";
 	public static String NAME_RUS = "Рабочая область";
