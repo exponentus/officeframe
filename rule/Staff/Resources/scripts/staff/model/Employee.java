51c65a02e92aa1e48db8a211215d543bca811b30
kayra <justaidajam@gmail.com>
2016-11-25 13:43:17 +0200

    #

--- rule/Staff/Resources/scripts/staff/model/Employee.java
+++ rule/Staff/Resources/scripts/staff/model/Employee.java
@@ -20,7 +20,6 @@ import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import org.eclipse.persistence.annotations.Cache;
-import org.eclipse.persistence.annotations.Customizer;
 import org.eclipse.persistence.config.CacheIsolationType;
 
 import com.exponentus.common.model.Attachment;
@@ -38,116 +37,114 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import administrator.model.User;
 import reference.model.Position;
-import staff.model.customizer.EmployeeCustomizer;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "employees", uniqueConstraints = @UniqueConstraint(columnNames = { "name", "organization_id" }))
 @NamedQuery(name = "Employee.findAll", query = "SELECT m FROM Employee AS m ORDER BY m.regDate")
 @Cache(isolation = CacheIsolationType.ISOLATED)
-@Customizer(EmployeeCustomizer.class)
 @JsonPropertyOrder({ "kind", "name" })
 public class Employee extends SimpleReferenceEntity implements IEmployee {
-	
+
 	@OneToOne(cascade = { CascadeType.MERGE }, optional = false, fetch = FetchType.EAGER)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
-	
+
 	@Column(name = "birth_date")
 	private Date birthDate;
-	
+
 	@FTSearchable
 	private String iin = "";
-	
+
 	@NotNull
 	@ManyToOne(optional = true)
 	@JoinColumn(nullable = false)
 	private Organization organization;
-	
+
 	@NotNull
 	@ManyToOne(optional = true)
 	@JoinColumn(nullable = false)
 	private Department department;
-	
+
 	@NotNull
 	@ManyToOne(optional = true)
 	@JoinColumn(nullable = false)
 	private Employee boss;
-	
+
 	@ManyToOne(optional = true)
 	@JoinColumn(nullable = false)
 	private Position position;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "employee_role")
 	private List<Role> roles;
-	
+
 	private int rank = 999;
-	
+
 	@JsonIgnore
 	@Embedded
 	private Avatar avatar;
-	
+
 	// @JsonIgnore
 	public Organization getOrganization() {
 		return organization;
 	}
-	
+
 	public void setOrganization(Organization organization) {
 		this.organization = organization;
 	}
-	
+
 	// @JsonIgnore
 	public Department getDepartment() {
 		return department;
 	}
-	
+
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
-	
+
 	public Employee getBoss() {
 		return boss;
 	}
-	
+
 	public void setBoss(Employee boss) {
 		this.boss = boss;
 	}
-	
+
 	// @JsonIgnore
 	public Position getPosition() {
 		return position;
 	}
-	
+
 	public void setPosition(Position position) {
 		this.position = position;
 	}
-	
+
 	public Date getBirthDate() {
 		return birthDate;
 	}
-	
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
-	
+
 	public String getIin() {
 		return iin;
 	}
-	
+
 	public void setIin(String iin) {
 		this.iin = iin;
 	}
-	
+
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
+
 	@JsonIgnore
 	public User getUser() {
 		return user;
 	}
-	
+
 	public String getLogin() {
 		if (user != null) {
 			return user.getLogin();
@@ -155,7 +152,7 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 			return UndefinedUser.USER_NAME;
 		}
 	}
-	
+
 	@Override
 	public Long getUserID() {
 		if (user != null) {
@@ -164,39 +161,39 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 			return UndefinedUser.ID;
 		}
 	}
-	
+
 	public List<Role> getRoles() {
 		return roles;
 	}
-	
+
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
 	}
-	
+
 	public void addRole(Role r) {
 		if (roles == null) {
 			roles = new ArrayList<>();
 		}
 		roles.add(r);
 	}
-	
+
 	public int getRank() {
 		return rank;
 	}
-	
+
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
-	
+
 	@JsonIgnore
 	public Avatar getAvatar() {
 		return avatar;
 	}
-	
+
 	public void setAvatar(Avatar avatar) {
 		this.avatar = avatar;
 	}
-	
+
 	@Override
 	public List<Attachment> getAttachments() {
 		List<Attachment> atts = new ArrayList<>();
@@ -206,7 +203,7 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 		atts.add(a);
 		return atts;
 	}
-	
+
 	@Override
 	public String getShortXMLChunk(_Session ses) {
 		StringBuilder chunk = new StringBuilder(1000);
@@ -215,28 +212,28 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 		chunk.append("<login>" + getLogin() + "</login>");
 		return chunk.toString();
 	}
-	
+
 	@Override
 	public List<String> getAllRoles() {
 		List<String> list = new ArrayList<>();
 		if (roles == null) {
 			return list;
 		}
-		
+
 		for (Role r : roles) {
 			list.add(r.getName());
 		}
 		return list;
 	}
-	
+
 	String getOrganizationId() {
 		return organization.getIdentifier();
 	}
-	
+
 	String getDepartmentId() {
 		return department.getIdentifier();
 	}
-	
+
 	@Override
 	public String getFullXMLChunk(_Session ses) {
 		StringBuilder chunk = new StringBuilder(1000);
@@ -247,9 +244,9 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 		if (user != null) {
 			chunk.append("<login>" + user.getLogin() + "</login>");
 		}
-		
+
 		chunk.append("<birthdate>" + TimeUtil.dateTimeToStringSilently(birthDate) + "</birthdate>");
-		
+
 		if (organization != null) {
 			chunk.append("<organization id=\"" + organization.getId() + "\">"
 					+ organization.getLocalizedName(ses.getLang()) + "</organization>");
@@ -258,12 +255,12 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 			chunk.append("<department id=\"" + department.getId() + "\">" + department.getLocalizedName(ses.getLang())
 					+ "</department>");
 		}
-		
+
 		if (position != null) {
 			chunk.append("<position id=\"" + position.getId() + "\">" + position.getLocalizedName(ses.getLang())
 					+ "</position>");
 		}
-		
+
 		chunk.append("<roles>");
 		if (roles != null) {
 			for (Role l : roles) {
@@ -271,8 +268,8 @@ public class Employee extends SimpleReferenceEntity implements IEmployee {
 			}
 		}
 		chunk.append("</roles>");
-		
+
 		return chunk.toString();
 	}
-	
+
 }
--- rule/Staff/Resources/scripts/staff/model/Organization.java
+++ rule/Staff/Resources/scripts/staff/model/Organization.java
@@ -17,8 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import org.eclipse.persistence.annotations.Customizer;
-
 import com.exponentus.common.model.SimpleReferenceEntity;
 import com.exponentus.dataengine.jpadatabase.ftengine.FTSearchable;
 import com.exponentus.scripting._Session;
@@ -26,92 +24,90 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import reference.model.OrgCategory;
-import staff.model.customizer.OrganizationCustomizer;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "orgs", uniqueConstraints = @UniqueConstraint(columnNames = { "name", "bin" }))
-@Customizer(OrganizationCustomizer.class)
 @NamedQuery(name = "Organization.findAll", query = "SELECT m FROM Organization AS m ORDER BY m.regDate")
 public class Organization extends SimpleReferenceEntity {
-	
+
 	@ManyToOne(optional = false)
 	@JoinColumn(nullable = false)
 	private OrgCategory orgCategory;
-	
+
 	@OneToMany(mappedBy = "organization")
 	private List<Department> departments;
-	
+
 	@OneToMany(mappedBy = "organization")
 	private List<Employee> employers;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "orgs_labels", joinColumns = @JoinColumn(name = "org_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "label_id", referencedColumnName = "id"))
 	private List<OrganizationLabel> labels;
-	
+
 	@FTSearchable
 	@Column(length = 12)
 	private String bin = "";
-	
+
 	private int rank = 999;
-	
+
 	@JsonIgnore
 	public OrgCategory getOrgCategory() {
 		return orgCategory;
 	}
-	
+
 	public void setOrgCategory(OrgCategory orgCategory) {
 		this.orgCategory = orgCategory;
 	}
-	
+
 	@JsonIgnore
 	public List<Department> getDepartments() {
 		return departments;
 	}
-	
+
 	@JsonIgnore
 	public List<Employee> getEmployers() {
 		return employers;
 	}
-	
+
 	public void setEmployers(List<Employee> employers) {
 		this.employers = employers;
 	}
-	
+
 	public String getBin() {
 		return bin;
 	}
-	
+
 	public void setBin(String bin) {
 		this.bin = bin;
 	}
-	
+
 	@JsonIgnore
 	public List<OrganizationLabel> getLabels() {
 		return labels;
 	}
-	
+
 	public void setLabels(List<OrganizationLabel> labels) {
 		this.labels = labels;
 	}
-	
+
 	public List<String> getOrgLabelIds() {
 		return Optional.of(labels).orElse(new ArrayList<>()).stream().map(it -> it.getIdentifier())
 				.collect(Collectors.toList());
 	}
-	
+
 	public int getRank() {
 		return rank;
 	}
-	
+
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
-	
+
 	public String getOrgCategoryId() {
 		return orgCategory != null ? orgCategory.getIdentifier() : "";
 	}
-	
+
 	@Override
 	public String getFullXMLChunk(_Session ses) {
 		StringBuilder chunk = new StringBuilder(1000);
@@ -126,7 +122,7 @@ public class Organization extends SimpleReferenceEntity {
 		chunk.append("</labels>");
 		return chunk.toString();
 	}
-	
+
 	@Override
 	public String getShortXMLChunk(_Session ses) {
 		StringBuilder chunk = new StringBuilder(1000);
