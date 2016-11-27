be02ef3ba762a467c90f1ba7117a4b86986d002b
kayra <justaidajam@gmail.com>
2016-11-27 22:21:05 +0200

    #

--- rule/Reference/Resources/scripts/reference/model/DocumentTypeCategory.java
+++ rule/Reference/Resources/scripts/reference/model/DocumentTypeCategory.java
@@ -7,14 +7,15 @@ import javax.persistence.Table;
 import com.exponentus.common.model.SimpleReferenceEntity;
 
 @Entity
-@Table(name = "document_type_category")
+@Table(name = "document_type_categories")
 @NamedQuery(name = "DocumentTypeCategory.findAll", query = "SELECT m FROM DocumentTypeCategory AS m ORDER BY m.regDate")
 public class DocumentTypeCategory extends SimpleReferenceEntity {
 	String code;
+	
 	public String getCode() {
 		return code;
 	}
-
+	
 	public void setCode(String code) {
 		this.code = code;
 	}
