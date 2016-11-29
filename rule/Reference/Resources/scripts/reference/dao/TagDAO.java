a1ac19091b53f46c1a2268124b7f9dba272d33e8
medin84 <kalihanovm@gmail.com>
2016-11-29 19:36:55 +0600

    tag dao

--- rule/Reference/Resources/scripts/reference/dao/TagDAO.java
+++ rule/Reference/Resources/scripts/reference/dao/TagDAO.java
@@ -1,44 +1,69 @@
 package reference.dao;
 
-import java.util.UUID;
+import com.exponentus.dataengine.RuntimeObjUtil;
+import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.dataengine.jpa.ViewPage;
+import com.exponentus.scripting._Session;
+import reference.model.Tag;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
-import com.exponentus.dataengine.exception.DAOException;
-import com.exponentus.scripting._Session;
-
-import reference.model.Tag;
+import java.util.List;
+import java.util.UUID;
 
 public class TagDAO extends ReferenceDAO<Tag, UUID> {
-	
-	public TagDAO(_Session session) throws DAOException {
-		super(Tag.class, session);
-	}
-	
-	@Override
-	public Tag findByName(String tagName) {
-		EntityManager em = getEntityManagerFactory().createEntityManager();
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		try {
-			CriteriaQuery<Tag> cq = cb.createQuery(Tag.class);
-			Root<Tag> c = cq.from(Tag.class);
-			cq.select(c);
-			Predicate condition = cb.equal(c.get("name"), tagName);
-			cq.where(condition);
-			Query query = em.createQuery(cq);
-			Tag entity = (Tag) query.getSingleResult();
-			return entity;
-		} catch (NoResultException e) {
-			return null;
-		} finally {
-			em.close();
-		}
-	}
-	
+
+    public TagDAO(_Session session) throws DAOException {
+        super(Tag.class, session);
+    }
+
+    public ViewPage<Tag> findAllByCategoryAndVisibility(String categoryName, boolean withHidden, int pageNum, int pageSize) {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        try {
+            CriteriaQuery<Tag> cq = cb.createQuery(Tag.class);
+            CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+            Root<Tag> c = cq.from(Tag.class);
+            cq.select(c);
+            countCq.select(cb.count(c));
+
+            Predicate condition = null;
+
+            if (!categoryName.isEmpty()) {
+                condition = cb.like(cb.lower(c.<String>get("category")), categoryName);
+            }
+            if (!withHidden) {
+                if (condition == null) {
+                    condition = cb.isFalse(c.get("hidden"));
+                } else {
+                    condition = cb.and(cb.isFalse(c.get("hidden")), condition);
+                }
+            }
+
+            cq.where(condition);
+            countCq.where(condition);
+            TypedQuery<Tag> typedQuery = em.createQuery(cq);
+            Query query = em.createQuery(countCq);
+            long count = (long) query.getSingleResult();
+            int maxPage = 1;
+            if (pageNum != 0 || pageSize != 0) {
+                maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
+                if (pageNum == 0) {
+                    pageNum = maxPage;
+                }
+                int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
+                typedQuery.setFirstResult(firstRec);
+                typedQuery.setMaxResults(pageSize);
+            }
+            List<Tag> result = typedQuery.getResultList();
+            return new ViewPage<>(result, count, maxPage, pageNum);
+        } finally {
+            em.close();
+        }
+    }
 }
--- rule/Reference/Resources/scripts/reference/page/view/TagView.java
+++ rule/Reference/Resources/scripts/reference/page/view/TagView.java
@@ -1,9 +1,9 @@
 package reference.page.view;
 
-import java.util.UUID;
-
 import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.dataengine.jpa.ViewPage;
 import com.exponentus.exception.SecureException;
+import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.actions._Action;
@@ -12,52 +12,58 @@ import com.exponentus.scripting.actions._ActionType;
 import com.exponentus.scripting.event._DoPage;
 import com.exponentus.user.IUser;
 import com.exponentus.user.SuperUser;
-
 import reference.dao.TagDAO;
 import reference.model.Tag;
+
+import java.util.UUID;
 
 /**
  * @author Kayra created 28-01-2016
  */
 
 public class TagView extends _DoPage {
-	
-	@Override
-	public void doGET(_Session session, _WebFormData formData) {
-		IUser<Long> user = session.getUser();
-		try {
-			if (user.getId() == SuperUser.ID || user.getRoles().contains("reference_admin")) {
-				_ActionBar actionBar = new _ActionBar(session);
-				_Action newDocAction = new _Action(getLocalizedWord("new_", session.getLang()), "", "new_tag");
-				newDocAction.setURL("Provider?id=tag-form");
-				actionBar.addAction(newDocAction);
-				actionBar.addAction(new _Action(getLocalizedWord("del_document", session.getLang()), "",
-						_ActionType.DELETE_DOCUMENT));
 
-				addContent(actionBar);
-			}
-			addContent(getViewPage(new TagDAO(session), formData));
-		} catch (DAOException e) {
-			logError(e);
-			setBadRequest();
+    @Override
+    public void doGET(_Session session, _WebFormData formData) {
+        IUser<Long> user = session.getUser();
+        try {
+            if (user.getId() == SuperUser.ID || user.getRoles().contains("reference_admin")) {
+                _ActionBar actionBar = new _ActionBar(session);
+                _Action newDocAction = new _Action(getLocalizedWord("new_", session.getLang()), "", "new_tag");
+                newDocAction.setURL("Provider?id=tag-form");
+                actionBar.addAction(newDocAction);
+                actionBar.addAction(new _Action(getLocalizedWord("del_document", session.getLang()), "",
+                        _ActionType.DELETE_DOCUMENT));
+                addContent(actionBar);
+            }
 
-		}
-	}
-	
-	@Override
-	public void doDELETE(_Session session, _WebFormData formData) {
-		try {
-			TagDAO dao = new TagDAO(session);
-			for (String id : formData.getListOfValuesSilently("docid")) {
-				Tag m = dao.findById(UUID.fromString(id));
-				
-				dao.delete(m);
-				
-			}
-		} catch (DAOException | SecureException e) {
-			logError(e);
-			setBadRequest();
+            String category = formData.getValueSilently("category");
+            if (category.isEmpty() || (!"software_developing_task".equals(category)) && !"software_developing_demand".equals(category)) {
+                addContent(getViewPage(new TagDAO(session), formData));
+            } else {
+                boolean withHidden = formData.getBoolSilently("hidden");
+                TagDAO dao = new TagDAO(session);
+                ViewPage<Tag> vp = dao.findAllByCategoryAndVisibility(category, withHidden, 0, 0);
+                _POJOListWrapper<Tag> plw = new _POJOListWrapper(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum(), session);
+                addContent(plw);
+            }
+        } catch (DAOException e) {
+            logError(e);
+            setBadRequest();
+        }
+    }
 
-		}
-	}
+    @Override
+    public void doDELETE(_Session session, _WebFormData formData) {
+        try {
+            TagDAO dao = new TagDAO(session);
+            for (String id : formData.getListOfValuesSilently("docid")) {
+                Tag m = dao.findById(UUID.fromString(id));
+                dao.delete(m);
+            }
+        } catch (DAOException | SecureException e) {
+            logError(e);
+            setBadRequest();
+        }
+    }
 }
