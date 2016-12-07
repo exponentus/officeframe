f4565c9013e13eb559c093daceee4d297ec90b5c
medin84 <kalihanovm@gmail.com>
2016-12-07 14:08:32 +0600

    fix vp tree

--- rule/Reference/Resources/scripts/reference/page/view/RegionView.java
+++ rule/Reference/Resources/scripts/reference/page/view/RegionView.java
@@ -1,10 +1,7 @@
 package reference.page.view;
 
-import java.util.UUID;
-
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.exception.SecureException;
-import com.exponentus.scripting._ColumnOptions;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.actions._Action;
@@ -13,54 +10,47 @@ import com.exponentus.scripting.actions._ActionType;
 import com.exponentus.scripting.event._DoPage;
 import com.exponentus.user.IUser;
 import com.exponentus.user.SuperUser;
-
 import reference.dao.RegionDAO;
 import reference.model.Region;
 
+import java.util.UUID;
+
 public class RegionView extends _DoPage {
-	
-	@Override
-	public void doGET(_Session session, _WebFormData formData) {
-		IUser<Long> user = session.getUser();
-		try {
-			if (user.getId() == SuperUser.ID || user.getRoles().contains("reference_admin")) {
-				_ActionBar actionBar = new _ActionBar(session);
-				_Action newDocAction = new _Action(getLocalizedWord("new_", session.getLang()), "", "new_region");
-				newDocAction.setURL("Provider?id=region-form");
-				actionBar.addAction(newDocAction);
-				actionBar.addAction(new _Action(getLocalizedWord("del_document", session.getLang()), "",
-						_ActionType.DELETE_DOCUMENT));
 
-				addContent(actionBar);
-			}
-			addContent(getViewPage(new RegionDAO(session), formData));
+    @Override
+    public void doGET(_Session session, _WebFormData formData) {
+        IUser<Long> user = session.getUser();
+        try {
+            if (user.getId() == SuperUser.ID || user.getRoles().contains("reference_admin")) {
+                _ActionBar actionBar = new _ActionBar(session);
+                _Action newDocAction = new _Action(getLocalizedWord("new_", session.getLang()), "", "new_region");
+                newDocAction.setURL("Provider?id=region-form");
+                actionBar.addAction(newDocAction);
+                actionBar.addAction(new _Action(getLocalizedWord("del_document", session.getLang()), "",
+                        _ActionType.DELETE_DOCUMENT));
 
-			_ColumnOptions columnOptions = new _ColumnOptions();
-			columnOptions.add("name", "name", "localizedName", "both", "vw-name");
-			columnOptions.add("type", "type", "localizedName", "both", "");
+                addContent(actionBar);
+            }
+            addContent(getViewPage(new RegionDAO(session), formData));
+        } catch (DAOException e) {
+            logError(e);
+            setBadRequest();
+        }
+    }
 
-			addContent("columnOptions", columnOptions);
-		} catch (DAOException e) {
-			logError(e);
-			setBadRequest();
+    @Override
+    public void doDELETE(_Session session, _WebFormData formData) {
+        try {
 
-		}
-	}
-	
-	@Override
-	public void doDELETE(_Session session, _WebFormData formData) {
-		try {
-			
-			RegionDAO dao = new RegionDAO(session);
-			for (String id : formData.getListOfValuesSilently("docid")) {
-				Region m = dao.findById(UUID.fromString(id));
-				dao.delete(m);
-				
-			}
-		} catch (DAOException | SecureException e) {
-			logError(e);
-			setBadRequest();
+            RegionDAO dao = new RegionDAO(session);
+            for (String id : formData.getListOfValuesSilently("docid")) {
+                Region m = dao.findById(UUID.fromString(id));
+                dao.delete(m);
 
-		}
-	}
+            }
+        } catch (DAOException | SecureException e) {
+            logError(e);
+            setBadRequest();
+        }
+    }
 }
--- rule/Staff/Resources/scripts/staff/page/view/OrganizationView.java
+++ rule/Staff/Resources/scripts/staff/page/view/OrganizationView.java
@@ -1,11 +1,8 @@
 package staff.page.view;
 
-import java.util.UUID;
-
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.exception.SecureException;
 import com.exponentus.localization.LanguageCode;
-import com.exponentus.scripting._ColumnOptions;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.actions._Action;
@@ -14,57 +11,50 @@ import com.exponentus.scripting.actions._ActionType;
 import com.exponentus.scripting.event._DoPage;
 import com.exponentus.user.IUser;
 import com.exponentus.user.SuperUser;
-
 import staff.dao.OrganizationDAO;
 import staff.model.Organization;
 
+import java.util.UUID;
+
 public class OrganizationView extends _DoPage {
-	
-	@Override
-	public void doGET(_Session session, _WebFormData formData) {
-		LanguageCode lang = session.getLang();
-		try {
-			IUser<Long> user = session.getUser();
-			if (user.getId() == SuperUser.ID || user.getRoles().contains("staff_admin")) {
-				_ActionBar actionBar = new _ActionBar(session);
-				_Action newDocAction = new _Action(getLocalizedWord("new_", lang), "", "new_organization");
-				newDocAction.setURL("Provider?id=organization-form");
-				actionBar.addAction(newDocAction);
-				actionBar.addAction(
-						new _Action(getLocalizedWord("del_document", lang), "", _ActionType.DELETE_DOCUMENT));
-				addContent(actionBar);
-			}
-			addContent(getViewPage(new OrganizationDAO(session), formData));
 
-			_ColumnOptions columnOptions = new _ColumnOptions();
-			columnOptions.add("name", "name", "localizedName", "both", "vw-name");
-			columnOptions.add("bin", "bin", "text", "both", "vw-bin");
+    @Override
+    public void doGET(_Session session, _WebFormData formData) {
+        LanguageCode lang = session.getLang();
+        try {
+            IUser<Long> user = session.getUser();
+            if (user.getId() == SuperUser.ID || user.getRoles().contains("staff_admin")) {
+                _ActionBar actionBar = new _ActionBar(session);
+                _Action newDocAction = new _Action(getLocalizedWord("new_", lang), "", "new_organization");
+                newDocAction.setURL("Provider?id=organization-form");
+                actionBar.addAction(newDocAction);
+                actionBar.addAction(
+                        new _Action(getLocalizedWord("del_document", lang), "", _ActionType.DELETE_DOCUMENT));
+                addContent(actionBar);
+            }
+            addContent(getViewPage(new OrganizationDAO(session), formData));
+        } catch (DAOException e) {
+            logError(e);
+            setBadRequest();
+        }
+    }
 
-			addContent("columnOptions", columnOptions);
-		} catch (DAOException e) {
-			logError(e);
-			setBadRequest();
-			return;
-		}
-	}
-	
-	@Override
-	public void doDELETE(_Session session, _WebFormData formData) {
-		println(formData);
-		try {
-			OrganizationDAO dao = new OrganizationDAO(session);
-			for (String id : formData.getListOfValuesSilently("docid")) {
-				Organization m = dao.findById(UUID.fromString(id));
-				try {
-					dao.delete(m);
-				} catch (SecureException | DAOException e) {
-					setError(e);
-				}
-			}
-		} catch (DAOException e) {
-			logError(e);
-			setBadRequest();
-			return;
-		}
-	}
+    @Override
+    public void doDELETE(_Session session, _WebFormData formData) {
+        println(formData);
+        try {
+            OrganizationDAO dao = new OrganizationDAO(session);
+            for (String id : formData.getListOfValuesSilently("docid")) {
+                Organization m = dao.findById(UUID.fromString(id));
+                try {
+                    dao.delete(m);
+                } catch (SecureException | DAOException e) {
+                    setError(e);
+                }
+            }
+        } catch (DAOException e) {
+            logError(e);
+            setBadRequest();
+        }
+    }
 }
