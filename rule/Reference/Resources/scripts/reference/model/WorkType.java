7b3c1dbd05c7cc22ea7e69bc83139437ebf006da
medin84 <medin_84@mail.ru>
2017-03-14 11:15:13 +0600

    ref

--- rule/Reference/Resources/scripts/reference/model/WorkType.java
+++ rule/Reference/Resources/scripts/reference/model/WorkType.java
@@ -1,20 +1,35 @@
 package reference.model;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Entity;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import com.exponentus.common.model.SimpleReferenceEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRootName;
+
+import javax.persistence.*;
+import java.util.List;
 
 @JsonRootName("worktype")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Cacheable(true)
-@Table(name = "work_types", uniqueConstraints = @UniqueConstraint(columnNames = { "name" }))
+@Table(name = "work_types", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 @NamedQuery(name = "WorkType.findAll", query = "SELECT m FROM WorkType AS m ORDER BY m.regDate")
 public class WorkType extends SimpleReferenceEntity {
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent")
+    private WorkType parent;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "parent", cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    private List<WorkType> children;
+
+    public WorkType getParent() {
+        return parent;
+    }
+
+    public void setParent(WorkType parent) {
+        this.parent = parent;
+    }
 }
