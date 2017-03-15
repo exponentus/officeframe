e19181f9dfde2adcbcd646cccac23db51a365405
medin84 <medin_84@mail.ru>
2017-03-15 18:38:16 +0600

    fix

--- rule/Workspace/Resources/scripts/workspace/page/form/Workspace.java
+++ rule/Workspace/Resources/scripts/workspace/page/form/Workspace.java
@@ -1,9 +1,9 @@
 package workspace.page.form;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import administrator.dao.ApplicationDAO;
+import administrator.dao.LanguageDAO;
+import administrator.model.Application;
+import administrator.model.embedded.UserApplication;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.env.EnvConst;
 import com.exponentus.env.Environment;
@@ -15,49 +15,48 @@ import com.exponentus.server.Server;
 import com.exponentus.user.AnonymousUser;
 import com.exponentus.user.IUser;
 
-import administrator.dao.ApplicationDAO;
-import administrator.dao.LanguageDAO;
-import administrator.model.Application;
-import administrator.model.embedded.UserApplication;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Workspace extends _DoPage {
 
-	@Override
-	public void doGET(_Session session, WebFormData formData) {
-		addValue("serverversion", EnvConst.SERVER_VERSION);
-		addValue("build", Server.compilationTime);
-		addValue("org", Environment.orgName);
-		try {
-			if (!session.getUser().getUserID().equalsIgnoreCase(AnonymousUser.USER_NAME)) {
-				IUser<Long> user = session.getUser();
-				ApplicationDAO aDao = new ApplicationDAO(session);
-				List<Application> aa = new ArrayList<Application>();
-				List<UserApplication> userApps = null;
-				if (user.isSuperUser()) {
-					aa = aDao.findAllActivated().getResult();
-				} else {
-					aa = user.getAllowedApps();
-					userApps = user.getUserApplications();
-				}
+    @Override
+    public void doGET(_Session session, WebFormData formData) {
+        addValue("serverversion", EnvConst.SERVER_VERSION);
+        addValue("build", Server.compilationTime);
+        addValue("org", Environment.orgName);
+        try {
+            if (!session.getUser().getUserID().equalsIgnoreCase(AnonymousUser.USER_NAME)) {
+                IUser<Long> user = session.getUser();
+                ApplicationDAO aDao = new ApplicationDAO(session);
+                List<Application> aa = new ArrayList<Application>();
+                List<UserApplication> userApps = null;
+                if (user.isSuperUser()) {
+                    aa = aDao.findAllActivated().getResult();
+                } else {
+                    aa = user.getAllowedApps();
+                    userApps = user.getUserApplications();
+                }
 
-				Application app = aDao.findByName(EnvConst.WORKSPACE_NAME);
-				if (app != null) {
-					aa.remove(app);
-				}
-				Collections.sort(aa, (left, right) -> left.getPosition() - right.getPosition());
-				addContent(aa);
-				if (userApps != null) {
-					addContent(new _POJOListWrapper<UserApplication>(userApps, session));
-				}
-				
-			} else {
-				setUnauthorized();
-			}
-			addContent(new LanguageDAO(session).findAllActivated());
-		} catch (DAOException e) {
-			logError(e);
-			setBadRequest();
-			return;
-		}
-	}
+                Application app = aDao.findByName(EnvConst.WORKSPACE_NAME);
+                if (app != null) {
+                    aa.remove(app);
+                }
+                // Collections.sort(aa, (left, right) -> left.getPosition() - right.getPosition());
+                addContent(aa);
+                if (userApps != null) {
+                    addContent(new _POJOListWrapper<UserApplication>(userApps, session));
+                }
+
+            } else {
+                setUnauthorized();
+            }
+            addContent(new LanguageDAO(session).findAllActivated());
+        } catch (DAOException e) {
+            logError(e);
+            setBadRequest();
+            return;
+        }
+    }
 }
