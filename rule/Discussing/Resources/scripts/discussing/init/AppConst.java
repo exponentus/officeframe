ae5fd337a01749eba71da9823f05f14a04c32463
kaira <biosubj@gmail.com>
2017-03-17 14:32:33 +0200

    #

--- rule/Discussing/Resources/scripts/discussing/init/AppConst.java
+++ rule/Discussing/Resources/scripts/discussing/init/AppConst.java
@@ -3,7 +3,7 @@ package discussing.init;
 import com.exponentus.common.init.DefaultAppConst;
 
 public class AppConst extends DefaultAppConst {
-	public static String MODULE_ID = "Discussing";
+	public static String NAME = "Discussing";
 	public static String NAME_ENG = "Discussings";
 	public static String NAME_RUS = "Обсуждения";
 	public static String NAME_KAZ = "Обсуждения";
