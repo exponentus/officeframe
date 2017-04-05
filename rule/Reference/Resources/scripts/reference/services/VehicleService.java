d84df1b7944985b230e79bb42eab1e60efec22bb
medin84 <medin_84@mail.ru>
2017-04-05 23:41:04 +0600

    vehicle service

---
+++ rule/Reference/Resources/scripts/reference/services/VehicleService.java
@@ -0,0 +1,175 @@
+package reference.services;
+
+import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.dataengine.jpa.ViewPage;
+import com.exponentus.env.EnvConst;
+import com.exponentus.exception.SecureException;
+import com.exponentus.rest.RestProvider;
+import com.exponentus.rest.outgoingdto.Outcome;
+import com.exponentus.scripting.SortParams;
+import com.exponentus.scripting.WebFormData;
+import com.exponentus.scripting._Session;
+import com.exponentus.scripting._Validation;
+import com.exponentus.scripting.actions._Action;
+import com.exponentus.scripting.actions._ActionBar;
+import com.exponentus.scripting.actions._ActionType;
+import com.exponentus.user.IUser;
+import reference.dao.VehicleDAO;
+import reference.model.Vehicle;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+@Path("vehicles")
+public class VehicleService extends RestProvider {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getViewPage() {
+        _Session session = getSession();
+        IUser<Long> user = session.getUser();
+        WebFormData params = getWebFormData();
+        int pageSize = session.pageSize;
+
+        try {
+            Outcome outcome = new Outcome();
+
+            SortParams sortParams = params.getSortParams(SortParams.desc("regDate"));
+            VehicleDAO dao = new VehicleDAO(session);
+            ViewPage<Vehicle> vp = dao.findViewPage(sortParams, params.getPage(), pageSize);
+
+            if (user.isSuperUser() || user.getRoles().contains("reference_admin")) {
+                _ActionBar actionBar = new _ActionBar(session);
+                actionBar.addAction(new _Action("new_", "", "new_"));
+                actionBar.addAction(new _Action("del_document", "", _ActionType.DELETE_DOCUMENT));
+                outcome.addPayload(actionBar);
+            }
+
+            outcome.setTitle("vehicles");
+            outcome.addPayload(vp);
+
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("id") String id) {
+        try {
+            _Session session = getSession();
+            Vehicle entity;
+            boolean isNew = "new".equals(id);
+
+            if (isNew) {
+                entity = new Vehicle();
+                entity.setName("");
+                entity.setAuthor(session.getUser());
+            } else {
+                VehicleDAO dao = new VehicleDAO(session);
+                entity = dao.findById(id);
+            }
+
+            //
+            _ActionBar actionBar = new _ActionBar(session);
+            actionBar.addAction(new _Action("close", "", _ActionType.CLOSE));
+            if (session.getUser().isSuperUser() || session.getUser().getRoles().contains("reference_admin")) {
+                actionBar.addAction(new _Action("save_close", "", _ActionType.SAVE_AND_CLOSE));
+            }
+
+            Outcome outcome = new Outcome();
+            outcome.addPayload(entity);
+            outcome.addPayload("kind", entity.getEntityKind());
+            outcome.addPayload(EnvConst.FSID_FIELD_NAME, getWebFormData().getFormSesId());
+            outcome.addPayload(actionBar);
+
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(Vehicle dto) {
+        dto.setId(null);
+        return save(dto);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") String id, Vehicle dto) {
+        dto.setId(UUID.fromString(id));
+        return save(dto);
+    }
+
+    public Response save(Vehicle dto) {
+        _Session session = getSession();
+        IUser<Long> user = session.getUser();
+
+        if (!user.isSuperUser() && !user.getRoles().contains("reference_admin")) {
+            return null;
+        }
+
+        try {
+            validate(dto);
+
+            VehicleDAO dao = new VehicleDAO(session);
+            Vehicle entity;
+
+            if (dto.isNew()) {
+                entity = new Vehicle();
+            } else {
+                entity = dao.findById(dto.getId());
+            }
+
+            // fill from dto
+            entity.setName(dto.getName());
+            entity.setLocName(dto.getLocName());
+
+            dao.save(entity);
+
+            Outcome outcome = new Outcome();
+            outcome.addPayload(entity);
+
+            return Response.ok(outcome).build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        } catch (_Validation.VException e) {
+            return responseValidationError(e.getValidation());
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") String id) {
+        try {
+            VehicleDAO dao = new VehicleDAO(getSession());
+            Vehicle entity = dao.findById(id);
+            if (entity != null) {
+                dao.delete(entity);
+            }
+            return Response.noContent().build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    private void validate(Vehicle entity) throws _Validation.VException {
+        _Validation ve = new _Validation();
+
+        if (entity.getName() == null || entity.getName().isEmpty()) {
+            ve.addError("name", "required", "field_is_empty");
+        }
+
+        ve.assertValid();
+    }
+}
