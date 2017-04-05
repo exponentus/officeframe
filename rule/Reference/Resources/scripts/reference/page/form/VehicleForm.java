11be1d7fd585d4aa570c3aa2c01c0c3b4eec8c97
John <john_198708@mail.ru>
2017-04-06 00:06:55 +0600

    commit

---
+++ rule/Reference/Resources/scripts/reference/page/form/VehicleForm.java
@@ -0,0 +1,96 @@
+package reference.page.form;
+
+import administrator.dao.LanguageDAO;
+import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.dataengine.exception.DAOExceptionType;
+import com.exponentus.exception.SecureException;
+import com.exponentus.localization.LanguageCode;
+import com.exponentus.scripting.WebFormData;
+import com.exponentus.scripting.WebFormException;
+import com.exponentus.scripting._Session;
+import com.exponentus.scripting._Validation;
+import com.exponentus.user.IUser;
+import reference.dao.VehicleDAO;
+import reference.model.Vehicle;
+
+import java.util.UUID;
+
+public class VehicleForm extends ReferenceForm {
+	
+	@Override
+	public void doGET(_Session session, WebFormData formData) {
+		String id = formData.getValueSilently("docid");
+		IUser<Long> user = session.getUser();
+		try {
+			Vehicle entity;
+			if (!id.isEmpty()) {
+				VehicleDAO dao = new VehicleDAO(session);
+				entity = dao.findById(UUID.fromString(id));
+			} else {
+				entity = (Vehicle) getDefaultEntity(user, new Vehicle());
+			}
+			addContent(entity);
+			addContent(new LanguageDAO(session).findAllActivated());
+
+			addContent(getSimpleActionBar(session));
+		} catch (DAOException e) {
+			logError(e);
+			setBadRequest();
+
+		}
+		
+	}
+	
+	@Override
+	public void doPOST(_Session session, WebFormData formData) {
+		try {
+			_Validation ve = validate(formData, session.getLang());
+			if (ve.hasError()) {
+				setBadRequest();
+				setValidation(ve);
+				return;
+			}
+			
+			String id = formData.getValueSilently("docid");
+			VehicleDAO dao = new VehicleDAO(session);
+			Vehicle entity;
+			boolean isNew = id.isEmpty();
+			
+			if (isNew) {
+				entity = new Vehicle();
+			} else {
+				entity = dao.findById(UUID.fromString(id));
+			}
+			
+			entity.setName(formData.getValue("name"));
+			entity.setLocName(getLocalizedNames(session, formData));
+			
+			try {
+				if (isNew) {
+					dao.add(entity);
+				} else {
+					dao.update(entity);
+				}
+			} catch (DAOException e) {
+				if (e.getType() == DAOExceptionType.UNIQUE_VIOLATION) {
+					ve = new _Validation();
+					ve.addError("name", "unique_error", getLocalizedWord("name_is_not_unique", session.getLang()));
+					setBadRequest();
+					setValidation(ve);
+					return;
+				} else {
+					throw e;
+				}
+			}
+		} catch (WebFormException | SecureException | DAOException e) {
+			logError(e);
+			setBadRequest();
+		}
+	}
+	
+	protected _Validation validate(WebFormData formData, LanguageCode lang) {
+		_Validation ve = simpleCheck("name");
+
+		return ve;
+	}
+}
---
+++ rule/Reference/Resources/scripts/reference/page/view/VehicleView.java
@@ -0,0 +1,35 @@
+package reference.page.view;
+
+import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.scripting.WebFormData;
+import com.exponentus.scripting._Session;
+import com.exponentus.scripting.actions._Action;
+import com.exponentus.scripting.actions._ActionBar;
+import com.exponentus.scripting.actions._ActionType;
+import com.exponentus.user.IUser;
+import reference.dao.VehicleDAO;
+
+public class VehicleView extends ReferenceView {
+
+	@Override
+	public void doGET(_Session session, WebFormData formData) {
+		IUser<Long> user = session.getUser();
+		try {
+			if (user.isSuperUser() || user.getRoles().contains("reference_admin")) {
+				_ActionBar actionBar = new _ActionBar(session);
+				_Action newDocAction = new _Action(getLocalizedWord("new_", session.getLang()), "", "new_vehicle");
+				newDocAction.setURL("p?id=vehicle-form");
+				actionBar.addAction(newDocAction);
+				actionBar.addAction(new _Action(getLocalizedWord("del_document", session.getLang()), "",
+						_ActionType.DELETE_DOCUMENT));
+				addContent(actionBar);
+			}
+			addContent(getViewPage(new VehicleDAO(session), formData));
+		} catch (DAOException e) {
+			logError(e);
+			setBadRequest();
+
+		}
+	}
+
+}
