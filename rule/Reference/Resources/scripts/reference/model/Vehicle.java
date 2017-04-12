f65f0054a3575ce11f22a6314366a4663b766500
medin84 <medin_84@mail.ru>
2017-04-12 10:31:46 +0600

    ref

--- rule/Reference/Resources/scripts/reference/model/Vehicle.java
+++ rule/Reference/Resources/scripts/reference/model/Vehicle.java
@@ -1,21 +1,25 @@
 package reference.model;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Entity;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import com.exponentus.common.model.SimpleReferenceEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRootName;
+
+import javax.persistence.*;
 
 @JsonRootName("vehicle")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Cacheable(true)
-@Table(name = "vehicles", uniqueConstraints = @UniqueConstraint(columnNames = { "name" }))
+@Table(name = "vehicles", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 @NamedQuery(name = "Vehicle.findAll", query = "SELECT m FROM Vehicle AS m ORDER BY m.regDate")
 public class Vehicle extends SimpleReferenceEntity {
+
+    public enum VehicleType {CAR, SUV, PEOPLE_CARRIERS, VAN}
+
+    public enum TRANSMISSION {AUTO, MANUAL}
+
+    public VehicleType vehicleType = VehicleType.CAR;
+    public TRANSMISSION transmission = TRANSMISSION.AUTO;
+    public int capacity = 5;
 
 }
