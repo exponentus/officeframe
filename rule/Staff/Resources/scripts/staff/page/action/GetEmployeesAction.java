858689e370ca8d38ff20bc9ce7bbaf804463696b
kaira <biosubj@gmail.com>
2017-04-12 13:04:34 +0300

    #

--- rule/Staff/Resources/scripts/staff/page/action/GetEmployeesAction.java
+++ rule/Staff/Resources/scripts/staff/page/action/GetEmployeesAction.java
@@ -2,8 +2,8 @@ package staff.page.action;
 
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.dataengine.jpa.ViewPage;
-import com.exponentus.scripting._Session;
 import com.exponentus.scripting.WebFormData;
+import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event._DoPage;
 
 import staff.dao.EmployeeDAO;
@@ -13,7 +13,7 @@ import staff.dao.EmployeeDAO;
  */
 
 public class GetEmployeesAction extends _DoPage {
-	
+
 	@Override
 	public void doGET(_Session ses, WebFormData formData) {
 		try {
