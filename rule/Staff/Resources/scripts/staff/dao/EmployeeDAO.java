5af98d87f2ddb55642f0334e70495d6b838e7d18
kaira <biosubj@gmail.com>
2017-04-17 22:29:53 +0300

    #

--- rule/Staff/Resources/scripts/staff/dao/EmployeeDAO.java
+++ rule/Staff/Resources/scripts/staff/dao/EmployeeDAO.java
@@ -1,5 +1,17 @@
 package staff.dao;
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import com.exponentus.dataengine.RuntimeObjUtil;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.dataengine.jpa.DAO;
@@ -11,198 +23,197 @@ import com.exponentus.runtimeobj.ISimpleAppEntity;
 import com.exponentus.scripting.SortParams;
 import com.exponentus.scripting._Session;
 import com.exponentus.user.IUser;
+
 import staff.dao.filter.EmployeeFilter;
 import staff.model.Employee;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.List;
-import java.util.UUID;
-
 public class EmployeeDAO extends DAO<Employee, UUID> implements IExtUserDAO {
-    private static ViewPage<Employee> allEmployee;
+	private static ViewPage<Employee> allEmployee;
 
-    public EmployeeDAO(_Session session) throws DAOException {
-        super(Employee.class, session);
-    }
+	public EmployeeDAO(_Session session) throws DAOException {
+		super(Employee.class, session);
+	}
 
-    public ViewPage<Employee> findAll(boolean reloadCache) {
-        if (allEmployee == null || reloadCache) {
-            allEmployee = findAll(0, 0);
-        }
-        return allEmployee;
-    }
+	public ViewPage<Employee> findAll(boolean reloadCache) {
+		if (allEmployee == null || reloadCache) {
+			allEmployee = findAll(0, 0);
+		}
+		return allEmployee;
+	}
 
-    public ViewPage<Employee> findAll(EmployeeFilter filter, SortParams sortParams, int pageNum, int pageSize) {
-        if (filter == null) {
-            throw new IllegalArgumentException("filter is null");
-        }
+	public ViewPage<Employee> findAll(EmployeeFilter filter, SortParams sortParams, int pageNum, int pageSize) {
+		if (filter == null) {
+			throw new IllegalArgumentException("filter is null");
+		}
 
-        EntityManager em = getEntityManagerFactory().createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        try {
-            CriteriaQuery<Employee> cq = cb.createQuery(getEntityClass());
-            CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
-            Root<Employee> root = cq.from(getEntityClass());
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		try {
+			CriteriaQuery<Employee> cq = cb.createQuery(getEntityClass());
+			CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+			Root<Employee> root = cq.from(getEntityClass());
 
-            Predicate condition = null;
+			Predicate condition = null;
 
-            if (filter.getRoles() != null && !filter.getRoles().isEmpty()) {
-                condition = cb.and(root.get("roles").in(filter.getRoles()));
-            }
+			if (filter.getRoles() != null && !filter.getRoles().isEmpty()) {
+				condition = cb.and(root.get("roles").in(filter.getRoles()));
+			}
 
-            if (filter.getKeyword() != null && !filter.getKeyword().isEmpty()) {
-                if (condition != null) {
-                    condition = cb.and(cb.like(cb.lower(root.<String>get("name")), "%" + filter.getKeyword().toLowerCase() + "%"), condition);
-                } else {
-                    condition = cb.like(cb.lower(root.<String>get("name")), "%" + filter.getKeyword().toLowerCase() + "%");
-                }
-            }
+			if (filter.getKeyword() != null && !filter.getKeyword().isEmpty()) {
+				if (condition != null) {
+					condition = cb.and(
+							cb.like(cb.lower(root.<String>get("name")), "%" + filter.getKeyword().toLowerCase() + "%"),
+							condition);
+				} else {
+					condition = cb.like(cb.lower(root.<String>get("name")),
+							"%" + filter.getKeyword().toLowerCase() + "%");
+				}
+			}
 
-            cq.select(root);
-            countCq.select(cb.count(root));
+			cq.select(root);
+			countCq.select(cb.count(root));
 
-            if (condition != null) {
-                cq.where(condition);
-                countCq.where(condition);
-            }
+			if (condition != null) {
+				cq.where(condition);
+				countCq.where(condition);
+			}
 
-            cq.orderBy(collectSortOrder(cb, root, sortParams));
+			cq.orderBy(collectSortOrder(cb, root, sortParams));
 
-            TypedQuery<Employee> typedQuery = em.createQuery(cq);
-            Query query = em.createQuery(countCq);
-            long count = (long) query.getSingleResult();
+			TypedQuery<Employee> typedQuery = em.createQuery(cq);
+			Query query = em.createQuery(countCq);
+			long count = (long) query.getSingleResult();
 
-            int maxPage = 1;
-            if (pageNum != 0 || pageSize != 0) {
-                maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
-                if (pageNum == 0) {
-                    pageNum = maxPage;
-                }
-                int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
-                typedQuery.setFirstResult(firstRec);
-                if (pageSize > 0) {
-                    typedQuery.setMaxResults(pageSize);
-                }
-            }
-            List<Employee> result = typedQuery.getResultList();
-            return new ViewPage<>(result, count, maxPage, pageNum);
-        } finally {
-            em.close();
-        }
-    }
+			int maxPage = 1;
+			if (pageNum != 0 || pageSize != 0) {
+				maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
+				if (pageNum == 0) {
+					pageNum = maxPage;
+				}
+				int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
+				typedQuery.setFirstResult(firstRec);
+				if (pageSize > 0) {
+					typedQuery.setMaxResults(pageSize);
+				}
+			}
+			List<Employee> result = typedQuery.getResultList();
+			return new ViewPage<>(result, count, maxPage, pageNum);
+		} finally {
+			em.close();
+		}
+	}
 
-    public Employee findByUser(IUser<Long> user) {
-        return findByUserId(user.getId());
-    }
+	public Employee findByUser(IUser<Long> user) {
+		return findByUserId(user.getId());
+	}
 
-    public Employee findByUserId(long id) {
-        EntityManager em = getEntityManagerFactory().createEntityManager();
-        try {
-            String jpql = "SELECT m FROM Employee AS m WHERE m.user.id = :id";
-            TypedQuery<Employee> q = em.createQuery(jpql, Employee.class);
-            q.setParameter("id", id);
-            List<Employee> res = q.getResultList();
-            return res.get(0);
-        } catch (IndexOutOfBoundsException e) {
-            return null;
-        } finally {
-            em.close();
-        }
-    }
+	public Employee findByUserId(long id) {
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		try {
+			String jpql = "SELECT m FROM Employee AS m WHERE m.user.id = :id";
+			TypedQuery<Employee> q = em.createQuery(jpql, Employee.class);
+			q.setParameter("id", id);
+			List<Employee> res = q.getResultList();
+			return res.get(0);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		} finally {
+			em.close();
+		}
+	}
 
-    public List<Employee> findAllByUserIds(List<Long> ids) {
-        EntityManager em = getEntityManagerFactory().createEntityManager();
-        try {
-            String jpql = "SELECT m FROM Employee AS m WHERE m.user.id in :ids";
-            TypedQuery<Employee> q = em.createQuery(jpql, Employee.class);
-            q.setParameter("ids", ids);
-            return q.getResultList();
-        } catch (IndexOutOfBoundsException e) {
-            return null;
-        } finally {
-            em.close();
-        }
-    }
+	public List<Employee> findAllByUserIds(List<Long> ids) {
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		try {
+			String jpql = "SELECT m FROM Employee AS m WHERE m.user.id in :ids";
+			TypedQuery<Employee> q = em.createQuery(jpql, Employee.class);
+			q.setParameter("ids", ids);
+			return q.getResultList();
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		} finally {
+			em.close();
+		}
+	}
 
-    public ViewPage<Employee> findAllByName(String keyword, int pageNum, int pageSize) {
-        EntityManager em = getEntityManagerFactory().createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        try {
-            CriteriaQuery<Employee> cq = cb.createQuery(getEntityClass());
-            CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
-            Root<Employee> c = cq.from(getEntityClass());
-            cq.select(c);
-            countCq.select(cb.count(c));
-            Predicate condition = cb.like(cb.lower(c.<String>get("name")), "%" + keyword.toLowerCase() + "%");
-            cq.where(condition);
-            countCq.where(condition);
-            TypedQuery<Employee> typedQuery = em.createQuery(cq);
-            Query query = em.createQuery(countCq);
-            long count = (long) query.getSingleResult();
-            int maxPage = 1;
-            if (pageNum != 0 || pageSize != 0) {
-                maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
-                if (pageNum == 0) {
-                    pageNum = maxPage;
-                }
-                int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
-                typedQuery.setFirstResult(firstRec);
-                typedQuery.setMaxResults(pageSize);
-            }
-            List<Employee> result = typedQuery.getResultList();
-            return new ViewPage<>(result, count, maxPage, pageNum);
-        } finally {
-            em.close();
-        }
-    }
+	public ViewPage<Employee> findAllByName(String keyword, int pageNum, int pageSize) {
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		try {
+			CriteriaQuery<Employee> cq = cb.createQuery(getEntityClass());
+			CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+			Root<Employee> c = cq.from(getEntityClass());
+			cq.select(c);
+			countCq.select(cb.count(c));
+			Predicate condition = cb.like(cb.lower(c.<String>get("name")), "%" + keyword.toLowerCase() + "%");
+			cq.where(condition);
+			countCq.where(condition);
+			TypedQuery<Employee> typedQuery = em.createQuery(cq);
+			Query query = em.createQuery(countCq);
+			long count = (long) query.getSingleResult();
+			int maxPage = 1;
+			if (pageNum != 0 || pageSize != 0) {
+				maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
+				if (pageNum == 0) {
+					pageNum = maxPage;
+				}
+				int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
+				typedQuery.setFirstResult(firstRec);
+				typedQuery.setMaxResults(pageSize);
+			}
+			List<Employee> result = typedQuery.getResultList();
+			return new ViewPage<>(result, count, maxPage, pageNum);
+		} finally {
+			em.close();
+		}
+	}
 
-    @Override
-    public IEmployee getEmployee(long id) {
-        return findByUserId(id);
-    }
+	@Override
+	public IEmployee getEmployee(long id) {
+		return findByUserId(id);
+	}
 
-    @Override
-    public Employee add(Employee entity) throws SecureException, DAOException {
-        Employee e = super.add(entity);
-        allEmployee = null;
-        getEntityManagerFactory().getCache().evict(Employee.class);
-        return e;
-    }
+	@Override
+	public IEmployee getEmployee(UUID id) {
+		return findById(id);
+	}
 
-    @Override
-    public Employee update(Employee entity) throws SecureException, DAOException {
-        Employee e = super.update(entity);
-        allEmployee = null;
-        getEntityManagerFactory().getCache().evict(Employee.class);
-        return e;
-    }
+	@Override
+	public Employee add(Employee entity) throws SecureException, DAOException {
+		Employee e = super.add(entity);
+		allEmployee = null;
+		getEntityManagerFactory().getCache().evict(Employee.class);
+		return e;
+	}
 
-    @Override
-    public void delete(ISimpleAppEntity<UUID> entity) throws SecureException, DAOException {
-        EntityManager em = getEntityManagerFactory().createEntityManager();
-        try {
-            EntityTransaction t = em.getTransaction();
-            try {
-                t.begin();
-                entity = em.merge(entity);
-                em.remove(entity);
-                t.commit();
-            } finally {
-                if (t.isActive()) {
-                    t.rollback();
-                }
-                getEntityManagerFactory().getCache().evict(Employee.class);
-            }
-        } finally {
-            em.close();
-        }
-        allEmployee = null;
-    }
+	@Override
+	public Employee update(Employee entity) throws SecureException, DAOException {
+		Employee e = super.update(entity);
+		allEmployee = null;
+		getEntityManagerFactory().getCache().evict(Employee.class);
+		return e;
+	}
+
+	@Override
+	public void delete(ISimpleAppEntity<UUID> entity) throws SecureException, DAOException {
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		try {
+			EntityTransaction t = em.getTransaction();
+			try {
+				t.begin();
+				entity = em.merge(entity);
+				em.remove(entity);
+				t.commit();
+			} finally {
+				if (t.isActive()) {
+					t.rollback();
+				}
+				getEntityManagerFactory().getCache().evict(Employee.class);
+			}
+		} finally {
+			em.close();
+		}
+		allEmployee = null;
+	}
+
 }
