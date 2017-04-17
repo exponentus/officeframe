d4aa8c5e679fa7e431874ad2bc7bffeb787c05ff
medin84 <medin_84@mail.ru>
2017-04-17 09:23:39 +0600

    ws page

--- rule/Workspace/Resources/scripts/workspace/page/Workspace.java
+++ rule/Workspace/Resources/scripts/workspace/page/Workspace.java
@@ -1,20 +1,18 @@
 package workspace.page;
 
-import com.exponentus.env.EnvConst;
 import com.exponentus.env.Environment;
 import com.exponentus.scripting.WebFormData;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event._DoPage;
-import com.exponentus.server.Server;
 import com.exponentus.user.AnonymousUser;
 
 public class Workspace extends _DoPage {
 
+    private static final long tm = System.currentTimeMillis();
+
     @Override
     public void doGET(_Session session, WebFormData formData) {
         addValue("logo", Environment.orgLogo);
-        addValue("server_version", EnvConst.SERVER_VERSION);
-        addValue("build", Server.compilationTime);
 
         if (session.getUser().getUserID().equalsIgnoreCase(AnonymousUser.USER_NAME)) {
             setUnauthorized();
