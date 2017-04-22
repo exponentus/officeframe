35af8b070c8ff1bda524fcc1f614a2249b59e4b0
kaira <biosubj@gmail.com>
2017-04-22 21:56:13 +0300

    #

--- rule/Staff/Resources/scripts/staff/task/RestoreOfficeframe.java
+++ rule/Staff/Resources/scripts/staff/task/RestoreOfficeframe.java
@@ -17,24 +17,25 @@ import com.exponentus.runtimeobj.IAppEntity;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event._Do;
 import com.exponentus.scriptprocessor.tasks.Command;
-import com.exponentus.user.SuperUser;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import administrator.dao.UserDAO;
 import administrator.model.User;
 import administrator.model.embedded.UserApplication;
+import reference.dao.DepartmentTypeDAO;
 import reference.dao.OrgCategoryDAO;
+import reference.dao.PositionDAO;
+import reference.model.DepartmentType;
 import reference.model.OrgCategory;
+import reference.model.Position;
+import staff.dao.DepartmentDAO;
+import staff.dao.EmployeeDAO;
 import staff.dao.OrganizationDAO;
+import staff.model.Department;
+import staff.model.Employee;
 import staff.model.Organization;
 
 @Command(name = "restore_of")
@@ -44,12 +45,12 @@ public class RestoreOfficeframe extends _Do {
 	public void doTask(AppEnv appEnv, _Session ses) {
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		restoreUser();
-		// restore(ses, OrgCategory.class);
-		// restore(ses, DepartmentType.class);
-		// restore(ses, Position.class);
-		// restoreOrganization(ses);
-		// restore(ses, Department.class);
-		// restore(ses, Employee.class);
+		restore(ses, OrgCategory.class);
+		restore(ses, DepartmentType.class);
+		restore(ses, Position.class);
+		restoreOrganization(ses);
+		restoreDepartment(ses);
+		restoreEmployee(ses);
 	}
 
 	private <T extends IAppEntity<UUID>> void restore(_Session ses, Class<T> class1) {
@@ -88,7 +89,6 @@ public class RestoreOfficeframe extends _Do {
 
 	private <T extends IAppEntity<UUID>> void restoreOrganization(_Session ses) {
 		BufferedReader reader = getFile(Organization.class);
-		// mapper.addMixIn(Organization.class, OrganizationMixIn.class);
 		try {
 			OrganizationDAO dao = new OrganizationDAO(ses);
 
@@ -103,6 +103,101 @@ public class RestoreOfficeframe extends _Do {
 					newOrg.setOrgCategory(new OrgCategoryDAO(ses).findByName(entity.getOrgCategory().getName()));
 					dao.add(newOrg);
 					System.out.println("\"" + newOrg + "\" backup was restored");
+
+				} catch (DAOException e) {
+					if (e.getType() == DAOExceptionType.UNIQUE_VIOLATION) {
+						logger.warningLogEntry("a data is already exists (" + e.getAddInfo() + "), record was skipped");
+					} else if (e.getType() == DAOExceptionType.NOT_NULL_VIOLATION) {
+						logger.warningLogEntry("a value is null (" + e.getAddInfo() + "), record was skipped");
+					} else {
+						logger.errorLogEntry(e);
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	private <T extends IAppEntity<UUID>> void restoreDepartment(_Session ses) {
+		BufferedReader reader = getFile(Department.class);
+		try {
+			DepartmentDAO dao = new DepartmentDAO(ses);
+
+			String line = "";
+			reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				try {
+					Department entity = mapper.readValue(line, Department.class);
+					Department newEntity = new Department();
+					newEntity.setName(entity.getName());
+					newEntity.setLocName(entity.getLocName());
+					newEntity.setType(new DepartmentTypeDAO(ses).findByName(entity.getType().getName()));
+					List<Organization> list = new OrganizationDAO(ses)
+							.findAllByKeyword(newEntity.getOrganization().getName(), 1, 1).getResult();
+					if (list.size() > 0) {
+						newEntity.setOrganization(list.get(1));
+					}
+					dao.add(newEntity);
+					System.out.println("\"" + newEntity + "\" backup was restored");
+
+				} catch (DAOException e) {
+					if (e.getType() == DAOExceptionType.UNIQUE_VIOLATION) {
+						logger.warningLogEntry("a data is already exists (" + e.getAddInfo() + "), record was skipped");
+					} else if (e.getType() == DAOExceptionType.NOT_NULL_VIOLATION) {
+						logger.warningLogEntry("a value is null (" + e.getAddInfo() + "), record was skipped");
+					} else {
+						logger.errorLogEntry(e);
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	private <T extends IAppEntity<UUID>> void restoreEmployee(_Session ses) {
+		BufferedReader reader = getFile(Employee.class);
+		try {
+			EmployeeDAO dao = new EmployeeDAO(ses);
+
+			String line = "";
+			reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				try {
+					Employee entity = mapper.readValue(line, Employee.class);
+					Employee newEntity = new Employee();
+					newEntity.setName(entity.getName());
+					newEntity.setLocName(entity.getLocName());
+					List<Organization> list = new OrganizationDAO(ses)
+							.findAllByKeyword(newEntity.getOrganization().getName(), 1, 1).getResult();
+					if (list.size() > 0) {
+						newEntity.setOrganization(list.get(1));
+					}
+					List<Department> depList = new DepartmentDAO(ses)
+							.findAllequal("name", newEntity.getDepartment().getName(), 1, 1).getResult();
+					if (depList.size() > 0) {
+						newEntity.setDepartment(depList.get(1));
+					}
+					newEntity.setPosition(new PositionDAO(ses).findByName(entity.getPosition().getName()));
+					dao.add(newEntity);
+					System.out.println("\"" + newEntity + "\" backup was restored");
 
 				} catch (DAOException e) {
 					if (e.getType() == DAOExceptionType.UNIQUE_VIOLATION) {
@@ -174,25 +269,4 @@ public class RestoreOfficeframe extends _Do {
 		UserApplication userApplications;
 	}
 
-	abstract class OrganizationMixIn {
-		@JsonDeserialize(using = OrgCategoryDeserializer.class)
-		OrgCategory orgCategory;
-	}
-
-	public class OrgCategoryDeserializer extends JsonDeserializer<OrgCategory> {
-
-		@Override
-		public OrgCategory deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-				throws IOException, JsonProcessingException {
-			JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-			String name = node.get("name").asText();
-			OrgCategoryDAO dao = (OrgCategoryDAO) DAOFactory.get(new _Session(new SuperUser()), "orgcategory");
-			try {
-				return dao.findByName(name);
-			} catch (DAOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-	}
 }
