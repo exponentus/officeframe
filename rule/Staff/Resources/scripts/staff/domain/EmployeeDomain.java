a376ec2dbe0c72ab0c8625308c34e3129bb39430
kaira <biosubj@gmail.com>
2017-06-21 18:51:15 +0300

    #

--- rule/Staff/Resources/scripts/staff/domain/EmployeeDomain.java
+++ rule/Staff/Resources/scripts/staff/domain/EmployeeDomain.java
@@ -1,6 +1,6 @@
 package staff.domain;
 
-import com.exponentus.common.domain.DTOService;
+import com.exponentus.common.domain.CommonDomain;
 import com.exponentus.common.domain.IValidation;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.rest.validation.exception.DTOException;
@@ -9,7 +9,7 @@ import com.exponentus.scripting._Session;
 import staff.dao.EmployeeDAO;
 import staff.model.Employee;
 
-public class EmployeeDomain extends DTOService<Employee> {
+public class EmployeeDomain extends CommonDomain<Employee> {
 
 	protected EmployeeDomain(_Session ses) throws DAOException {
 		super(ses);
