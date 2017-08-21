790da4dca156ca331f927f2590e3c05f2c5a66ec
kaira <biosubj@gmail.com>
2017-08-21 18:41:23 +0300

    #

--- rule/Reference/Resources/scripts/reference/model/PropertyCode.java
+++ rule/Reference/Resources/scripts/reference/model/PropertyCode.java
@@ -1,20 +1,27 @@
 package reference.model;
 
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import com.exponentus.common.model.SimpleReferenceEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
-import javax.persistence.*;
-
 /**
+ * Сущность право пользования
+ * 
  * @author Kayra created 07-01-2016
+ * 
  */
 
 @JsonRootName("propertyCode")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Cacheable(true)
-@Table(name = "ref__property_codes", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
+@Table(name = "ref__property_codes", uniqueConstraints = @UniqueConstraint(columnNames = { "name" }))
 @NamedQuery(name = "PropertyCode.findAll", query = "SELECT m FROM PropertyCode AS m ORDER BY m.regDate")
 public class PropertyCode extends SimpleReferenceEntity {
 }
