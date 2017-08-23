dc9184ff1f4815a0918e75370fda9e97e04be3b3
kaira <biosubj@gmail.com>
2017-08-23 20:02:46 +0300

    #

--- rule/Reference/Resources/scripts/reference/init/AppConst.java
+++ rule/Reference/Resources/scripts/reference/init/AppConst.java
@@ -13,8 +13,7 @@ public class AppConst extends DefaultAppConst {
 	public static String NAME_POR = "Referências";
 	public static String NAME_SPA = "Referencias";
 	public static String BASE_URL = "/" + NAME + "/";
-	public static InterfaceType AVAILABLE_MODE[] = { InterfaceType.HTML, InterfaceType.SPA };
-	public static String DEFAULT_PAGE = "country-view";
+	public static InterfaceType AVAILABLE_MODE[] = { InterfaceType.SPA };
 	public static final String[] ROLES = { "reference_admin" };
 	public static boolean FORCE_DEPLOYING = true;
 }
--- rule/Staff/Resources/scripts/staff/init/AppConst.java
+++ rule/Staff/Resources/scripts/staff/init/AppConst.java
@@ -12,8 +12,7 @@ public class AppConst extends DefaultAppConst {
 	public static String NAME_POR = "Funcionários";
 	public static String NAME_SPA = "Personal";
 	public static String BASE_URL = "/" + NAME + "/";
-	public static InterfaceType AVAILABLE_MODE[] = { InterfaceType.HTML, InterfaceType.SPA };
-	public static String DEFAULT_PAGE = "organization-view";
+	public static InterfaceType AVAILABLE_MODE[] = {InterfaceType.SPA };
 	public static final String[] ORG_LABELS = { "primary", "inactive" };
 	public static final String[] ROLES = { "acting", "staff_admin", "fired", "senior_manager" };
 	public static boolean FORCE_DEPLOYING = true;
