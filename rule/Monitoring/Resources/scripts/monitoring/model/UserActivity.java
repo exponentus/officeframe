76dd307c057169141ba3b7ea5ce3fa26da81b986
medin84 <medin_84@mail.ru>
2017-08-28 10:50:50 +0600

    c

--- rule/Monitoring/Resources/scripts/monitoring/model/UserActivity.java
+++ rule/Monitoring/Resources/scripts/monitoring/model/UserActivity.java
@@ -80,6 +80,6 @@ public class UserActivity extends SimpleAppEntity {
 
     @Override
     public String getURL() {
-        return AppConst.BASE_URL + "services/" + getIdentifier();
+        return AppConst.BASE_URL + "user-activities/" + getIdentifier();
     }
 }
