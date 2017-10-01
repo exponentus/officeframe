f00fa3963b8c61460b0d9c0e8a9fb39b59857056
John <john_198708@mail.ru>
2017-10-02 00:47:51 +0600

    #commit

--- rule/Reference/Resources/scripts/reference/services/BuildingStatesService.java
+++ rule/Reference/Resources/scripts/reference/services/BuildingStatesService.java
@@ -12,10 +12,8 @@ import com.exponentus.scripting.WebFormData;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.actions._ActionBar;
 import com.exponentus.user.IUser;
-import reference.dao.RealEstateObjStatusDAO;
 import reference.model.BuildingState;
-import reference.model.RealEstateObjStatus;
-import reference.model.TaxIncomeCategory;
+import reference.dao.BuildingStateDAO;
 import reference.ui.Action;
 
 import javax.ws.rs.*;
@@ -28,5 +26,158 @@ import static reference.init.AppConst.ROLE_REFERENCE_ADMIN;
 @Path("building-states")
 public class BuildingStatesService extends ReferenceService<BuildingState> {
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getViewPage() {
+        _Session session = getSession();
+        IUser user = session.getUser();
+        WebFormData params = getWebFormData();
+        int pageSize = session.getPageSize();
+
+        try {
+            Outcome outcome = new Outcome();
+
+            SortParams sortParams = params.getSortParams(SortParams.desc("regDate"));
+            BuildingStateDAO dao = new BuildingStateDAO(session);
+            ViewPage<BuildingState> vp = dao.findViewPage(sortParams, params.getPage(), pageSize);
+
+            if (user.isSuperUser() || user.getRoles().contains(ROLE_REFERENCE_ADMIN)) {
+                _ActionBar actionBar = new _ActionBar(session);
+                Action action = new Action();
+                actionBar.addAction(action.addNew);
+                actionBar.addAction(action.deleteDocument);
+                actionBar.addAction(action.refreshVew);
+                outcome.addPayload(actionBar);
+            }
+
+            outcome.setTitle("building_states");
+            outcome.addPayload("contentTitle", "building_states");
+            outcome.addPayload(vp);
+
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("id") String id) {
+        try {
+            _Session session = getSession();
+            BuildingState entity;
+            boolean isNew = "new".equals(id);
+
+            if (isNew) {
+                entity = new BuildingState();
+                entity.setName("");
+                entity.setAuthor(session.getUser());
+            } else {
+                BuildingStateDAO dao = new BuildingStateDAO(session);
+                entity = dao.findByIdentefier(id);
+            }
+
+            //
+            _ActionBar actionBar = new _ActionBar(session);
+            actionBar.addAction(new Action().close);
+            if (session.getUser().isSuperUser() || session.getUser().getRoles().contains(ROLE_REFERENCE_ADMIN)) {
+                actionBar.addAction(new Action().saveAndClose);
+            }
+
+            Outcome outcome = new Outcome();
+            outcome.addPayload(entity.getEntityKind(), entity);
+            outcome.addPayload("kind", entity.getEntityKind());
+            outcome.addPayload("contentTitle", "building_state");
+            outcome.addPayload(EnvConst.FSID_FIELD_NAME, getWebFormData().getFormSesId());
+            outcome.addPayload(actionBar);
+
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(BuildingState dto) {
+        dto.setId(null);
+        return save(dto);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") String id, BuildingState dto) {
+        dto.setId(UUID.fromString(id));
+        return save(dto);
+    }
+
+    public Response save(BuildingState dto) {
+        _Session session = getSession();
+        IUser user = session.getUser();
+
+        if (!user.isSuperUser() && !user.getRoles().contains(ROLE_REFERENCE_ADMIN)) {
+            return null;
+        }
+
+        try {
+            validate(dto);
+
+            BuildingStateDAO dao = new BuildingStateDAO(session);
+            BuildingState entity;
+
+            if (dto.isNew()) {
+                entity = new BuildingState();
+            } else {
+                entity = dao.findById(dto.getId());
+            }
+
+            // fill from dto
+            entity.setName(dto.getName());
+            entity.setLocName(dto.getLocName());
+
+            dao.save(entity);
+
+            Outcome outcome = new Outcome();
+            outcome.addPayload(entity);
+
+            return Response.ok(outcome).build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        } catch (DTOException e) {
+            return responseValidationError(e);
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") String id) {
+        try {
+            BuildingStateDAO dao = new BuildingStateDAO(getSession());
+            BuildingState entity = dao.findByIdentefier(id);
+            if (entity != null) {
+                dao.delete(entity);
+            }
+            return Response.noContent().build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    private void validate(BuildingState entity) throws DTOException {
+        DTOException ve = new DTOException();
+
+        if (entity.getName() == null || entity.getName().isEmpty()) {
+            ve.addError("name", "required", "field_is_empty");
+        }
+
+        if (ve.hasError()) {
+            throw ve;
+        }
+    }
 
 }
