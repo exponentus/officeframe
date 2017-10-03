29c2b9e7a50604e5115f5284f6ab495bea7c531f
medin84 <medin_84@mail.ru>
2017-10-03 13:16:40 +0600

    c

---
+++ rule/Discussing/Resources/scripts/discussing/services/NavigatorService.java
@@ -0,0 +1,36 @@
+package discussing.services;
+
+import com.exponentus.rest.RestProvider;
+import com.exponentus.rest.outgoingdto.Outcome;
+import com.exponentus.scripting.outline.Outline;
+import com.exponentus.scripting.outline.OutlineEntry;
+import com.exponentus.scriptprocessor.page.IOutcomeObject;
+import discussing.init.AppConst;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.LinkedList;
+
+@Path("navigator")
+public class NavigatorService extends RestProvider {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getNav() {
+
+        Outline co = new Outline("", "common");
+        co.addEntry(new OutlineEntry("topics_all", "", "topics_all", AppConst.BASE_URL + "topics"));
+        co.addEntry(new OutlineEntry("topics_my", "", "topics_my", AppConst.BASE_URL + "topics/s/my"));
+
+        LinkedList<IOutcomeObject> list = new LinkedList<>();
+        list.add(co);
+
+        Outcome outcome = new Outcome();
+        outcome.addPayload("nav", list);
+
+        return Response.ok(outcome).build();
+    }
+}
