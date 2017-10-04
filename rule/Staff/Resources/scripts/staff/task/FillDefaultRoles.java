b65c1bfd076ff9055679964a8b380827d844a882
kaira <biosubj@gmail.com>
2017-10-04 18:55:48 +0100

    #

--- rule/Staff/Resources/scripts/staff/task/FillDefaultRoles.java
+++ rule/Staff/Resources/scripts/staff/task/FillDefaultRoles.java
@@ -1,29 +1,18 @@
 package staff.task;
 
 import com.exponentus.appenv.AppEnv;
-import com.exponentus.dataengine.exception.DAOException;
-import com.exponentus.dataengine.exception.DAOExceptionType;
-import com.exponentus.exception.SecureException;
-import com.exponentus.localization.constants.LanguageCode;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event.Do;
 import com.exponentus.scriptprocessor.constants.Trigger;
 import com.exponentus.scriptprocessor.tasks.Command;
 import staff.init.AppConst;
-import staff.dao.RoleDAO;
-import staff.model.Role;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Command(name = AppConst.CODE + "_fill_roles", trigger = Trigger.POST_APP_DEPLOYED)
 public class FillDefaultRoles extends Do {
 
 	@Override
 	public void doTask(AppEnv appEnv, _Session ses) {
-		addDefaultRoles(appEnv);
+		addApplicationRoles(appEnv);
 	}
 
 }
