edf0446ea2241ea3821b5a215a05bdf6b9ec9bb6
medin84 <medin_84@mail.ru>
2017-10-24 20:11:04 +0600

    c

--- rule/Reference/Resources/scripts/reference/model/District.java
+++ rule/Reference/Resources/scripts/reference/model/District.java
@@ -2,7 +2,6 @@ package reference.model;
 
 import com.exponentus.common.model.SimpleReferenceEntity;
 import com.exponentus.common.model.converter.ListOfStringArrayConverter;
-import com.exponentus.common.model.converter.LocalizedValConverter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import reference.init.AppConst;
@@ -32,11 +31,9 @@ public class District extends SimpleReferenceEntity {
         return localities;
     }
 
-
     @Convert(converter = ListOfStringArrayConverter.class)
     @Column(name = "lat_lng", columnDefinition = "jsonb")
-    public List<String[]> latLng = new ArrayList<>();
-
+    private List<String[]> latLng = new ArrayList<>();
 
     public Region getRegion() {
         return region;
