687ee36919032126fcee01a85c5f3333abf158c7
kaira <biosubj@gmail.com>
2017-11-01 17:16:11 +0000

    #

--- rule/Workspace/Resources/scripts/workspace/init/AppConst.java
+++ rule/Workspace/Resources/scripts/workspace/init/AppConst.java
@@ -6,12 +6,14 @@ import administrator.model.constants.InterfaceType;
 import administrator.model.constants.VisibiltyMode;
 
 public class AppConst extends DefaultAppConst {
-	public static String CODE = "ws";
+	public static final String CODE = "ws";
 	public static VisibiltyMode VISIBILITY = VisibiltyMode.HIDDEN;
 	public static String NAME = "Workspace";
 	public static String NAME_ENG = "Workspace";
 	public static String NAME_RUS = "Рабочая область";
 	public static String NAME_KAZ = "Жұмыс аумағы";
+	public static String NAME_POR = "Área de trabalho";
+	public static String NAME_SPA = "Espacio de trabajo";
 	public static final InterfaceType AVAILABLE_MODE[] = { InterfaceType.SPA };
 	public static boolean FORCE_DEPLOYING = true;
 
--- rule/Workspace/Resources/scripts/workspace/task/WriteAppCacheManifest.java
+++ rule/Workspace/Resources/scripts/workspace/task/WriteAppCacheManifest.java
@@ -8,6 +8,7 @@ import com.exponentus.scripting.event.Do;
 import com.exponentus.scriptprocessor.constants.Trigger;
 import com.exponentus.scriptprocessor.tasks.Command;
 import com.exponentus.server.Server;
+import workspace.init.AppConst;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -15,7 +16,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@Command(name = "wr_app_cache_manifest", trigger = Trigger.POST_APP_START)
+@Command(name = AppConst.CODE + "_wr_app_cache_manifest", trigger = Trigger.POST_APP_START)
 public class WriteAppCacheManifest extends Do {
 
     @Override
