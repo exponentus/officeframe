4e1f255d277926392480b7d05db4972eac59c949
kaira <biosubj@gmail.com>
2017-11-06 20:47:47 +0000

    #

--- rule/Reference/Resources/scripts/reference/services/EngInfrastructObjClassificationService.java
+++ rule/Reference/Resources/scripts/reference/services/EngInfrastructObjClassificationService.java
@@ -1,26 +1,8 @@
 package reference.services;
 
-import com.exponentus.common.ui.ConventionalActionFactory;
-import com.exponentus.common.ui.ViewPage;
-import com.exponentus.dataengine.exception.DAOException;
-import com.exponentus.env.EnvConst;
-import com.exponentus.exception.SecureException;
-import com.exponentus.rest.RestProvider;
-import com.exponentus.rest.outgoingdto.Outcome;
-import com.exponentus.rest.validation.exception.DTOException;
-import com.exponentus.scripting.SortParams;
-import com.exponentus.scripting.WebFormData;
-import com.exponentus.scripting._Session;
-import com.exponentus.user.IUser;
-import reference.dao.LandClassificationDAO;
 import reference.model.EngInfrastructObjClassification;
-import reference.model.ExpenditureCategory;
-import reference.model.LandClassification;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.UUID;
+import javax.ws.rs.Path;
 
 
 @Path("eng-infrastruct-obj-classifications")
