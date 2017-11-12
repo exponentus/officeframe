7442cc312d07bd2b2905c8e6eae8e71a004f8893
John <john_198708@mail.ru>
2017-11-12 21:26:53 +0600

    #commit

--- rule/Reference/Resources/scripts/reference/services/RoadTypeService.java
+++ rule/Reference/Resources/scripts/reference/services/RoadTypeService.java
@@ -1,11 +1,145 @@
 package reference.services;
 
+import reference.dao.RoadTypeDAO;
 import reference.model.RoadType;
 
 import javax.ws.rs.Path;
 
 @Path("road-types")
 public class RoadTypeService extends ReferenceService<RoadType> {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getViewPage() {
+        _Session session = getSession();
+        IUser user = session.getUser();
+        WebFormData params = getWebFormData();
+        int pageSize = session.getPageSize();
 
+        try {
+            Outcome outcome = new Outcome();
+
+            SortParams sortParams = params.getSortParams(SortParams.desc("regDate"));
+            RoadTypeDAO dao = new RoadTypeDAO(session);
+
+            ViewPage<RoadType> vp = dao.findViewPage(sortParams, params.getPage(), pageSize);
+            outcome.addPayload(getDefaultViewActionBar(true));
+            outcome.setTitle("road_types");
+            outcome.addPayload("contentTitle", "road_types");
+            outcome.addPayload(vp);
+
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("id") String id) {
+        try {
+            _Session session = getSession();
+            RoadType entity;
+            boolean isNew = "new".equals(id);
+
+            if (isNew) {
+                entity = new RoadType();
+                entity.setName("");
+                entity.setAuthor(session.getUser());
+            } else {
+                RoadTypeDAO dao = new RoadTypeDAO(session);
+                entity = dao.findByIdentifier(id);
+            }
+
+
+            Outcome outcome = new Outcome();
+            outcome.addPayload(entity.getEntityKind(), entity);
+            outcome.addPayload("kind", entity.getEntityKind());
+            outcome.addPayload("contentTitle", "road_type");
+            outcome.addPayload(EnvConst.FSID_FIELD_NAME, getWebFormData().getFormSesId());
+            outcome.addPayload(getDefaultFormActionBar(entity));
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(RoadType dto) {
+        dto.setId(null);
+        return save(dto);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") String id, RoadType dto) {
+        dto.setId(UUID.fromString(id));
+        return save(dto);
+    }
+
+    public Response save(RoadType dto) {
+        _Session session = getSession();
+
+
+        try {
+            validate(dto);
+
+            RoadTypeDAO dao = new RoadTypeDAO(session);
+            RoadType entity;
+
+            if (dto.isNew()) {
+                entity = new RoadType();
+            } else {
+                entity = dao.findById(dto.getId());
+            }
+
+            // fill from dto
+            entity.setName(dto.getName());
+            entity.setLocName(dto.getLocName());
+
+            dao.save(entity);
+
+            Outcome outcome = new Outcome();
+            outcome.addPayload(entity);
+
+            return Response.ok(outcome).build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        } catch (DTOException e) {
+            return responseValidationError(e);
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") String id) {
+        try {
+            RoadTypeDAO dao = new RoadTypeDAO(getSession());
+            RoadType entity = dao.findById(id);
+            if (entity != null) {
+                dao.delete(entity);
+            }
+            return Response.noContent().build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        }
+    }
+
+    private void validate(RoadType entity) throws DTOException {
+        DTOException ve = new DTOException();
+
+        if (entity.getName() == null || entity.getName().isEmpty()) {
+            ve.addError("name", "required", "field_is_empty");
+        }
+
+        if (ve.hasError()) {
+            throw ve;
+        }
+    }
 
 }
