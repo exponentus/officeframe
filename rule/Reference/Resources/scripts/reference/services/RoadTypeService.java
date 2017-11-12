5d3c4854ef4601eb75fc8ecc49632c9f7795166b
John <john_198708@mail.ru>
2017-11-12 21:36:25 +0600

    #commit

--- rule/Reference/Resources/scripts/reference/services/RoadTypeService.java
+++ rule/Reference/Resources/scripts/reference/services/RoadTypeService.java
@@ -1,9 +1,23 @@
 package reference.services;
 
+import com.exponentus.common.ui.ViewPage;
+import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.env.EnvConst;
+import com.exponentus.exception.SecureException;
+import com.exponentus.rest.RestProvider;
+import com.exponentus.rest.outgoingdto.Outcome;
+import com.exponentus.rest.validation.exception.DTOException;
+import com.exponentus.scripting.SortParams;
+import com.exponentus.scripting.WebFormData;
+import com.exponentus.scripting._Session;
+import com.exponentus.user.IUser;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
 import reference.dao.RoadTypeDAO;
 import reference.model.RoadType;
 
-import javax.ws.rs.Path;
 
 @Path("road-types")
 public class RoadTypeService extends ReferenceService<RoadType> {
