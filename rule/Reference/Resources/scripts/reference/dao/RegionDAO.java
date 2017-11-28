7e7912ae50dbaddf37aadcb547fca0e5dec11ff9
kaira <biosubj@gmail.com>
2017-11-28 15:20:08 +0000

    #

--- rule/Reference/Resources/scripts/reference/dao/RegionDAO.java
+++ rule/Reference/Resources/scripts/reference/dao/RegionDAO.java
@@ -1,13 +1,21 @@
 package reference.dao;
 
 import com.exponentus.common.ui.ViewPage;
+import com.exponentus.dataengine.RuntimeObjUtil;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.scripting._Session;
+import reference.model.Country;
+import reference.model.Locality;
 import reference.model.Region;
 import staff.model.Organization;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,6 +33,39 @@ public class RegionDAO extends ReferenceDAO<Region, UUID> {
             return (Region) query.getResultList().get(0);
         } catch (IndexOutOfBoundsException e) {
             return null;
+        }
+    }
+
+    public ViewPage<Region> findAllByCountry(Country country, int pageNum, int pageSize) {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        try {
+            CriteriaQuery<Region> cq = cb.createQuery(getEntityClass());
+            CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+            Root<Region> c = cq.from(getEntityClass());
+            cq.select(c);
+            countCq.select(cb.count(c));
+            Predicate condition = cb.equal(c.get("country"), country);
+            cq.where(condition);
+            countCq.where(condition);
+
+            TypedQuery<Region> typedQuery = em.createQuery(cq);
+            Query query = em.createQuery(countCq);
+            long count = (long) query.getSingleResult();
+            int maxPage = 1;
+            if (pageNum != 0 || pageSize != 0) {
+                maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
+                if (pageNum == 0) {
+                    pageNum = maxPage;
+                }
+                int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
+                typedQuery.setFirstResult(firstRec);
+                typedQuery.setMaxResults(pageSize);
+            }
+            List<Region> result = typedQuery.getResultList();
+            return new ViewPage<Region>(result, count, maxPage, pageNum);
+        } finally {
+            em.close();
         }
     }
 }
