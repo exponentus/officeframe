7aaff0ce05983ebb5609fa239ff2abd480565d6a
kaira <biosubj@gmail.com>
2018-03-14 09:34:16 +0000

    #

---
+++ modules/Reference/Resources/scripts/reference/model/ProjectStage.java
@@ -0,0 +1,17 @@
+package reference.model;
+
+import com.exponentus.common.model.SimpleReferenceEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import reference.init.ModuleConst;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = ModuleConst.CODE + "__project_stages", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
+public class ProjectStage extends SimpleReferenceEntity {
+
+
+}
