af48d530fe32cf05d8c5273418d93f0839d1269a
kaira <biosubj@gmail.com>
2018-03-20 04:36:40 +0000

    #

--- modules/Reference/Resources/scripts/reference/task/FillOperativeInfoTypes.java
+++ modules/Reference/Resources/scripts/reference/task/FillOperativeInfoTypes.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+//run task ref_fill_operative_info_types
 @Command(name = ModuleConst.CODE + "_fill_operative_info_types")
 public class FillOperativeInfoTypes extends Do {
 
