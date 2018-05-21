04a8dff6ea9cbc246ddc12390e78319be34fa84c
medin84 <medin_84@mail.ru>
2018-05-21 14:49:52 +0600

    dev2477

--- modules/Reference/Resources/scripts/reference/services/AsOfService.java
+++ modules/Reference/Resources/scripts/reference/services/AsOfService.java
@@ -1,14 +1,19 @@
 package reference.services;
 
+import com.exponentus.common.ui.ViewPage;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.exception.SecureException;
 import com.exponentus.rest.outgoingdto.Outcome;
 import com.exponentus.rest.validation.exception.DTOException;
+import com.exponentus.scripting.SortParams;
+import com.exponentus.scripting.WebFormData;
 import com.exponentus.scripting._Session;
 import org.apache.commons.collections4.MapUtils;
 import reference.dao.AsOfDAO;
 import reference.model.AsOf;
+import reference.ui.ViewOptions;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,6 +23,30 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class AsOfService extends ReferenceService<AsOf> {
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getViewPage() {
+        _Session session = getSession();
+        WebFormData params = getWebFormData();
+        int pageSize = session.getPageSize();
+
+        try {
+            SortParams sortParams = params.getSortParams(SortParams.desc("asOfByDate"));
+            AsOfDAO dao = new AsOfDAO(session);
+            ViewPage<AsOf> vp = dao.findViewPage(sortParams, params.getPage(), pageSize);
+            vp.setViewPageOptions(new ViewOptions().getAsOfOptions());
+
+            Outcome outcome = new Outcome();
+            outcome.setTitle("as_of");
+            outcome.setPayloadTitle("as_of");
+            outcome.addPayload(getDefaultViewActionBar(true));
+            outcome.addPayload(vp);
+
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
 
     public Response save(AsOf dto) {
         _Session session = getSession();
