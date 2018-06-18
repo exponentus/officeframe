c96fcf324d9949a3d5a0c29950ecbaf554af89a8
John <john_198708@mail.ru>
2018-06-18 16:25:04 +0600

    #commit

--- modules/Reference/Resources/scripts/reference/model/EngInfrastructObjClassification.java
+++ modules/Reference/Resources/scripts/reference/model/EngInfrastructObjClassification.java
@@ -4,15 +4,13 @@ import com.exponentus.common.model.SimpleReferenceEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import reference.init.ModuleConst;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Cacheable(true)
-@Table(name = ModuleConst.CODE + "__route_classifications", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
+@Table(name = ModuleConst.CODE + "__eng_infrastruct_obj_classifications", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
+@NamedQuery(name = "EngInfrastructObjClassification.findAll", query = "SELECT m FROM EngInfrastructObjClassification AS m ORDER BY m.regDate")
 public class EngInfrastructObjClassification extends SimpleReferenceEntity {
 
     @Override
