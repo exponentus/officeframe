54253186d2899366bdf520c0fecec35230e0b10e
medin84 <medin_84@mail.ru>
2018-09-24 21:36:02 +0600

    c

--- webapps/Calendar/ng-app/src/app/calendar/calendar.module.ts
+++ webapps/Calendar/ng-app/src/app/calendar/calendar.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule as ngCalendarModule } from 'angular-calendar';
+import { CalendarModule as ngCalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { NbCommonModule } from '@nb/core';
 import { NbAclModule, NbEntityCreationDetailsModule } from '@nb/components';
@@ -29,7 +30,10 @@ import { ReminderFormComponent } from './components/reminder/reminder-form';
         NbAclModule,
         NbEntityCreationDetailsModule,
         RouterModule.forChild(CALENDAR_ROUTES),
-        ngCalendarModule.forRoot()
+        ngCalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory
+        })
     ],
     providers: [
         CalendarService
